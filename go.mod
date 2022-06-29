db8fb508cff26d267ddf80385ba23750f67deeb7
Antoine Gelloz <antoine.gelloz@me.com>
2022-06-29 19:38:54 +0200

    Test Ingest from Benthos (#6)

--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module github.com/numary/webhooks-cloud
 go 1.18
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/svix/svix-webhooks v0.60.0
 )
 
 require (
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/stretchr/testify v1.7.2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	svix "github.com/svix/svix-webhooks/go"
 )
 
 const (
 	defaultBind = ":8080"
 
 	healthCheckPath = "/_healthcheck"
+
+	svixServer       = "https://api.eu.svix.com"
+	svixAPIKeyEnvVar = "SVIX_API_KEY_DEV"
 )
 
 var Version = "v0.0"
@@ -30,6 +37,34 @@ func main() {
 			w.WriteHeader(http.StatusOK)
 		}),
 	)
+
+	if err := godotenv.Load(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+	}
+
+	svixToken, ok := os.LookupEnv(svixAPIKeyEnvVar)
+	if !ok {
+		_, _ = fmt.Fprintln(os.Stderr, fmt.Errorf(
+			"could not start the application: %s env var was not found", svixAPIKeyEnvVar))
+		os.Exit(1)
+	}
+
+	serverUrl, _ := url.Parse(svixServer)
+	svixClient := svix.New(svixToken, &svix.SvixOptions{
+		ServerUrl: serverUrl,
+	})
+	spew.Dump(svixClient)
+
+	var svixAppID = "app_2BEv2hBcE2ICiB6hq1QOVTVBWgF"
+	app, err := svixClient.Application.GetOrCreate(&svix.ApplicationIn{
+		Name: "Formance Webhooks Application",
+		Uid:  &svixAppID,
+	})
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	spew.Dump(app)
 
 	logger.Infof("starting http server on address: %s", defaultBind)
 	if err := http.ListenAndServe(defaultBind, router); err != nil {
