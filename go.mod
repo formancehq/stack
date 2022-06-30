e6be2e7e8af0e942de56d300dc0adca5af33093a
Antoine Gelloz <antoine.gelloz@me.com>
2022-06-30 16:13:40 +0200

    test ingest

--- go.mod
+++ go.mod
@@ -3,20 +3,11 @@ module github.com/numary/webhooks-cloud
 go 1.18
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.4.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/svix/svix-webhooks v0.60.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/stretchr/testify v1.7.2 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	golang.org/x/oauth2 v0.0.0-20220628200809-02e64fa58f26 // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,68 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
-	"net/url"
 	"os"
+	"syscall"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
-	svix "github.com/svix/svix-webhooks/go"
 )
 
 const (
 	defaultBind = ":8080"
 
 	healthCheckPath = "/_healthcheck"
-
-	svixServer = "https://api.eu.svix.com"
-	svixToken  = "testsk_CSkhagouqu-JXgZznr35dG2TYTmsCPnb"
+	ingestPath      = "/_ingest"
 )
 
 var Version = "v0.0"
 
-var svixAppID = "app_2BEv2hBcE2ICiB6hq1QOVTVBWgF"
+var logger = logrus.New()
 
 func main() {
-	fmt.Println("version:", Version)
-
-	logger := logrus.New()
 	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.Infof("app started with version: %s", Version)
+	logger.Infof("env: %+v", syscall.Environ())
 
 	router := mux.NewRouter()
-	router.Handle(healthCheckPath,
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Infof("health check OK")
-			w.WriteHeader(http.StatusOK)
-		}),
-	)
-
-	if err := godotenv.Load(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-	}
-
-	serverUrl, _ := url.Parse(svixServer)
-	svixClient := svix.New(svixToken, &svix.SvixOptions{
-		ServerUrl: serverUrl,
-	})
-	spew.Dump(svixClient)
-
-	app, err := svixClient.Application.GetOrCreate(&svix.ApplicationIn{
-		Name: "Formance Webhooks Application",
-		Uid:  &svixAppID,
-	})
-	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	spew.Dump(app)
+	router.HandleFunc(healthCheckPath, HealthCheckHandler)
+	router.HandleFunc(ingestPath, IngestHandler).Methods(http.MethodPost)
 
 	logger.Infof("starting http server on address: %s", defaultBind)
 	if err := http.ListenAndServe(defaultBind, router); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
+		logger.Errorf("http: %s", err)
 		os.Exit(1)
 	}
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Infof("health check OK")
+	w.WriteHeader(http.StatusOK)
+}
+
+func IngestHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		logger.Errorf("%s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	logger.Infof("OK:%s", string(b))
+	w.WriteHeader(http.StatusOK)
 }
