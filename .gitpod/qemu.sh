521ba4e316e51f0ba2b3b8c8f9b4de6a77909c19
flemzord <maxence@maireaux.fr>
2023-01-20 22:37:50 +0000

    feat(gitpod): Upgrade qemu

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -11,9 +11,9 @@ script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 outdir="${script_dirname}/_output"
 
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
--boot c -m 2049M -hda "${outdir}/rootfs/jammy-server-cloudimg-amd64.img" \
+-boot c -m 10240M -hda "${outdir}/rootfs/jammy-server-cloudimg-amd64.img" \
 -net user \
--smp 8 \
+-smp 6 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
 -nic user,hostfwd=tcp::2222-:22,hostfwd=tcp::6443-:6443,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443 \
 -serial mon:stdio -display none
