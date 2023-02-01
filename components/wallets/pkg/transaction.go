ddd37df32aedd7b9a9ed328d620188b90c09c46e
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-02-01 15:34:03 +0100

    fix: json marshalling

--- components/wallets/pkg/transaction.go
+++ components/wallets/pkg/transaction.go
@@ -1,10 +1,25 @@
 package wallet
 
 import (
+	"encoding/json"
+
 	sdk "github.com/formancehq/formance-sdk-go"
 )
 
 type Transaction struct {
 	sdk.Transaction
 	Ledger string `json:"ledger"`
+}
+
+func (t Transaction) MarshalJSON() ([]byte, error) {
+	asJSON, err := json.Marshal(t.Transaction)
+	if err != nil {
+		return nil, err
+	}
+	asMap := make(map[string]any)
+	if err := json.Unmarshal(asJSON, &asMap); err != nil {
+		return nil, err
+	}
+	asMap["ledger"] = t.Ledger
+	return json.Marshal(asMap)
 }
