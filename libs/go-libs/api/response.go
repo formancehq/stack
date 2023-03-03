07f5b4861170637f8129d8afae7b1bc25b1b72a5
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-03-03 14:03:58 +0100

    chore: remove 'total' property from cursors (#107)

--- libs/go-libs/api/response.go
+++ libs/go-libs/api/response.go
@@ -7,16 +7,10 @@ type BaseResponse[T any] struct {
 
 type Cursor[T any] struct {
 	PageSize int    `json:"pageSize,omitempty"`
-	Total    *Total `json:"total,omitempty"`
 	HasMore  bool   `json:"hasMore"`
 	Previous string `json:"previous,omitempty"`
 	Next     string `json:"next,omitempty"`
 	Data     []T    `json:"data"`
-}
-
-type Total struct {
-	Value uint64 `json:"value,omitempty"`
-	Rel   string `json:"relation,omitempty"`
 }
 
 type ErrorResponse struct {
