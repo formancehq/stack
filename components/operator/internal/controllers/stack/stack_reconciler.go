e023a3773d9bf14690aaefaf20a3dfe65de76521
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2023-03-10 12:48:00 +0100

    fix: missing access

--- components/operator/internal/controllers/stack/stack_reconciler.go
+++ components/operator/internal/controllers/stack/stack_reconciler.go
@@ -38,7 +38,7 @@ const (
 )
 
 // +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete;deletecollection
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
