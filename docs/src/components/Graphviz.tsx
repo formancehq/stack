bd073cb873ef8c5d0dafb783474d0987277b8564
jdupas22 <106673437+jdupas22@users.noreply.github.com>
2023-03-22 09:03:17 +0100

    docs: clarify and simplify language in Formance Flows documentation
    - Clarify language in Creating a Workflow section
    - Simplify language in Formance Flows introduction
    - Clarify language in Formance Flows compatibility model description
    - Simplify language in Formance Flows workflows capabilities description
    
    Signed-off-by: jdupas22 <106673437+jdupas22@users.noreply.github.com>

--- docs/src/components/Graphviz.tsx
+++ docs/src/components/Graphviz.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import Graphviz from 'graphviz-react';
 import React from 'react';
 
-const PostingsGraph = ({ postings, caption, additionnals }) => {
+const PostingsGraph = ({ postings, caption, additionnals =[] }) => {
   const graph : string[] = [];
 
   postings.map((posting) => {
