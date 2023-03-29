801ed4ddb5ea19170fd4cfb4e64616c2362c2d0c
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-03-29 14:08:11 +0200

    remove useless logging

--- components/ledger/pkg/ledger/monitor/monitor.go
+++ components/ledger/pkg/ledger/monitor/monitor.go
@@ -2,7 +2,6 @@ package monitor
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/formancehq/ledger/pkg/core"
 )
@@ -16,13 +15,10 @@ type Monitor interface {
 type noOpMonitor struct{}
 
 func (n noOpMonitor) CommittedTransactions(ctx context.Context, s string, res ...core.ExpandedTransaction) {
-	fmt.Println("FAKE MONITOR COMMITED TRANSACTIONS")
 }
 func (n noOpMonitor) SavedMetadata(ctx context.Context, ledger string, targetType string, id string, metadata core.Metadata) {
-	fmt.Println("FAKE MONITOR SAVE METADATA")
 }
 func (n noOpMonitor) RevertedTransaction(ctx context.Context, ledger string, reverted, revert *core.ExpandedTransaction) {
-	fmt.Println("FAKE MONITOR REVETED TRANSACTION")
 }
 
 var _ Monitor = &noOpMonitor{}
