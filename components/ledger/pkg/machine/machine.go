7db19e888aceaf58e657baeadb7706af1e23b894
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-03-30 16:10:25 +0200

    feat: rework all errors per package

--- components/ledger/pkg/machine/machine.go
+++ components/ledger/pkg/machine/machine.go
@@ -22,26 +22,9 @@ type Result struct {
 }
 
 func Run(m *vm.Machine, script core.RunScript) (*Result, error) {
-
-	exitCode, err := m.Execute()
+	err := m.Execute()
 	if err != nil {
 		return nil, errors.Wrap(err, "script execution failed")
-	}
-
-	if exitCode != vm.EXIT_OK {
-		switch exitCode {
-		case vm.EXIT_FAIL:
-			return nil, errors.New("script exited with error code EXIT_FAIL")
-		case vm.EXIT_FAIL_INVALID:
-			return nil, errors.New("internal error: compiled script was invalid")
-		case vm.EXIT_FAIL_INSUFFICIENT_FUNDS:
-			// TODO: If the machine can provide the asset which is failing
-			// we should be able to use InsufficientFundError{} instead of error code
-			return nil, vm.NewScriptError(vm.ScriptErrorInsufficientFund,
-				"account had insufficient funds")
-		default:
-			return nil, errors.New("script execution failed")
-		}
 	}
 
 	result := Result{
@@ -70,7 +53,7 @@ func Run(m *vm.Machine, script core.RunScript) (*Result, error) {
 	for k, v := range script.Metadata {
 		_, ok := result.Metadata[k]
 		if ok {
-			return nil, vm.NewScriptError(vm.ScriptErrorMetadataOverride, "cannot override metadata from script")
+			return nil, vm.ErrMetadataOverride
 		}
 		result.Metadata[k] = v
 	}
