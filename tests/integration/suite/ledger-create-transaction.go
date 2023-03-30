7d31fcdc84ac82ceee7eb886377d1debe00301bf
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-03-30 17:44:58 +0200

    feat: implement error handling utilities package (#194)

--- tests/integration/suite/ledger-create-transaction.go
+++ tests/integration/suite/ledger-create-transaction.go
@@ -185,7 +185,7 @@ var _ = Given("some empty environment", func() {
 			details := "https://play.numscript.org/?payload=eyJlcnJvciI6ImFjY291bnQgaGFkIGluc3VmZmljaWVudCBmdW5kcyJ9"
 			Expect(apiErr.Model()).Should(Equal(formance.ErrorResponse{
 				ErrorCode:    formance.INSUFFICIENT_FUND,
-				ErrorMessage: "[INSUFFICIENT_FUND] account had insufficient funds",
+				ErrorMessage: "account had insufficient funds",
 				Details:      &details,
 			}))
 		})
