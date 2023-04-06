3ea63f36f95bbabe98896f1310cb0a57957addf4
Maxence Maireaux <maxence@maireaux.fr>
2023-04-06 13:47:39 +0200

    chore: remove comment

--- components/auth/pkg/api/health.go
+++
@@ -1,8 +0,0 @@
-package api
-
-//func delegatedOIDCServerAvailability(rp rp.RelyingParty) health.NamedCheck {
-//	return health.NewNamedCheck("Delegated OIDC server", health.CheckFn(func(ctx context.Context) error {
-//		_, err := client.Discover(rp.Issuer(), http.DefaultClient)
-//		return err
-//	}))
-//}
