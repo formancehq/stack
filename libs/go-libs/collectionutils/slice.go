851d2f0a0b855cf4eae88e0067f6f70d57634929
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2023-04-17 16:38:16 +0200

    feat: merge all logs related writes

--- libs/go-libs/collectionutils/slice.go
+++ libs/go-libs/collectionutils/slice.go
@@ -1,9 +1,35 @@
 package collectionutils
 
+import (
+	"reflect"
+)
+
 func Map[FROM any, TO any](input []FROM, mapper func(FROM) TO) []TO {
 	ret := make([]TO, len(input))
 	for i, input := range input {
 		ret[i] = mapper(input)
 	}
 	return ret
+}
+
+func Filter[TYPE any](input []TYPE, filter func(TYPE) bool) []TYPE {
+	ret := make([]TYPE, 0)
+	for _, i := range input {
+		if filter(i) {
+			ret = append(ret, i)
+		}
+	}
+	return ret
+}
+
+func Eq[T any](t T) func(T) bool {
+	return func(t2 T) bool {
+		return reflect.DeepEqual(t, t2)
+	}
+}
+
+func Not[T any](t func(T) bool) func(T) bool {
+	return func(t2 T) bool {
+		return !t(t2)
+	}
 }
