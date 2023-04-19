c474efc0ae325b04e20ab0df099289cf828fdf76
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2023-04-19 16:06:40 +0200

    refactor: make commit process only use logs (not projections)

--- libs/go-libs/collectionutils/slice.go
+++ libs/go-libs/collectionutils/slice.go
@@ -22,14 +22,32 @@ func Filter[TYPE any](input []TYPE, filter func(TYPE) bool) []TYPE {
 	return ret
 }
 
-func Eq[T any](t T) func(T) bool {
+func First[TYPE any](input []TYPE, filter func(TYPE) bool) TYPE {
+	var zero TYPE
+	ret := Filter(input, filter)
+	if len(ret) >= 1 {
+		return ret[0]
+	}
+	return zero
+}
+
+func FilterEq[T any](t T) func(T) bool {
 	return func(t2 T) bool {
 		return reflect.DeepEqual(t, t2)
 	}
 }
 
-func Not[T any](t func(T) bool) func(T) bool {
+func FilterNot[T any](t func(T) bool) func(T) bool {
 	return func(t2 T) bool {
 		return !t(t2)
 	}
+}
+
+func Contains[T any](slice []T, t T) bool {
+	for _, t2 := range slice {
+		if reflect.DeepEqual(t, t2) {
+			return true
+		}
+	}
+	return false
 }
