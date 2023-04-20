913194a82a720d817971d5804532f3a7649fd80b
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2023-04-20 14:57:24 +0200

    feat: change status code from 200 to 201 on revert transactions

--- sdks/python/test/test_paths/test_api_ledger_ledger_transactions_txid_revert/test_post.py
+++ sdks/python/test/test_paths/test_api_ledger_ledger_transactions_txid_revert/test_post.py
@@ -32,7 +32,7 @@ class TestApiLedgerLedgerTransactionsTxidRevert(ApiTestMixin, unittest.TestCase)
     def tearDown(self):
         pass
 
-    response_status = 200
+    response_status = 201
 
 
 
