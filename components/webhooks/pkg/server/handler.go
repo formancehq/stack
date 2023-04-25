c5e2490ebe7a33cf4ebd391a042a307285e9a0e8
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-04-25 10:37:52 +0200

    feat: remove traces for health endpoint (webhooks)

--- components/webhooks/pkg/server/handler.go
+++ components/webhooks/pkg/server/handler.go
@@ -33,7 +33,6 @@ func newServerHandler(store storage.Store, httpClient *http.Client) http.Handler
 		httpClient: httpClient,
 	}
 
-	h.Mux.Use(otelchi.Middleware("webhooks"))
 	h.Mux.Use(func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
@@ -41,13 +40,18 @@ func newServerHandler(store storage.Store, httpClient *http.Client) http.Handler
 		})
 	})
 	h.Mux.Get(PathHealthCheck, h.HealthCheckHandle)
-	h.Mux.Get(PathConfigs, h.getManyConfigsHandle)
-	h.Mux.Post(PathConfigs, h.insertOneConfigHandle)
-	h.Mux.Delete(PathConfigs+PathId, h.deleteOneConfigHandle)
-	h.Mux.Get(PathConfigs+PathId+PathTest, h.testOneConfigHandle)
-	h.Mux.Put(PathConfigs+PathId+PathActivate, h.activateOneConfigHandle)
-	h.Mux.Put(PathConfigs+PathId+PathDeactivate, h.deactivateOneConfigHandle)
-	h.Mux.Put(PathConfigs+PathId+PathChangeSecret, h.changeSecretHandle)
+
+	h.Mux.Group(func(r chi.Router) {
+		r.Use(otelchi.Middleware("webhooks"))
+
+		h.Mux.Get(PathConfigs, h.getManyConfigsHandle)
+		h.Mux.Post(PathConfigs, h.insertOneConfigHandle)
+		h.Mux.Delete(PathConfigs+PathId, h.deleteOneConfigHandle)
+		h.Mux.Get(PathConfigs+PathId+PathTest, h.testOneConfigHandle)
+		h.Mux.Put(PathConfigs+PathId+PathActivate, h.activateOneConfigHandle)
+		h.Mux.Put(PathConfigs+PathId+PathDeactivate, h.deactivateOneConfigHandle)
+		h.Mux.Put(PathConfigs+PathId+PathChangeSecret, h.changeSecretHandle)
+	})
 
 	return h
 }
