1faa2f059e7f22f0c2558d1c0e9bba2fc584dd6c
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-05-23 12:24:20 +0200

    fix: fctl ledger v1 compatibility (#325)

--- components/fctl/cmd/ledger/internal/transaction.go
+++ components/fctl/cmd/ledger/internal/transaction.go
@@ -2,11 +2,15 @@ package internal
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"strconv"
 	"strings"
 
 	"github.com/formancehq/formance-sdk-go"
+	"github.com/formancehq/stack/libs/go-libs/api"
+	"github.com/formancehq/stack/libs/go-libs/collectionutils"
+	"golang.org/x/mod/semver"
 )
 
 func TransactionIDOrLastN(ctx context.Context, ledgerClient *formance.APIClient, ledger, id string) (int64, error) {
@@ -34,4 +38,32 @@ func TransactionIDOrLastN(ctx context.Context, ledgerClient *formance.APIClient,
 	}
 
 	return strconv.ParseInt(id, 10, 64)
+}
+
+func CreateTransaction(client *formance.APIClient, ctx context.Context, ledger string, transaction formance.PostTransaction) (*formance.Transaction, error) {
+
+	versions, _, err := client.DefaultApi.GetVersions(ctx).Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	version := collectionutils.Filter(versions.Versions, func(version formance.Version) bool {
+		return version.Name == "ledger"
+	})[0].Version
+
+	response, httpResponse, err := client.TransactionsApi.CreateTransaction(ctx, ledger).
+		PostTransaction(transaction).Execute()
+
+	if semver.IsValid(version) && semver.Compare(version, "v2.0.0") < 0 {
+		baseResponse := api.BaseResponse[[]formance.Transaction]{}
+		if err := json.NewDecoder(httpResponse.Body).Decode(&baseResponse); err != nil {
+			return nil, err
+		}
+		return &(*baseResponse.Data)[0], nil
+	} else {
+		if err != nil {
+			return nil, err
+		}
+		return &response.Data, nil
+	}
 }
