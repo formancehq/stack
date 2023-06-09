a5907ef84b4653780a2a892e49e65180f3c1610a
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-06-09 15:40:15 +0200

    fix: fix orchestration ledger v1/v2 compatibility

--- components/orchestration/internal/workflow/activities/activity_ledger_create_transaction.go
+++ components/orchestration/internal/workflow/activities/activity_ledger_create_transaction.go
@@ -2,13 +2,17 @@ package activities
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/formancehq/formance-sdk-go/pkg/models/operations"
 	"github.com/formancehq/formance-sdk-go/pkg/models/shared"
+	"github.com/formancehq/stack/libs/go-libs/api"
+	"github.com/formancehq/stack/libs/go-libs/collectionutils"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
+	"golang.org/x/mod/semver"
 )
 
 type CreateTransactionRequest struct {
@@ -17,6 +21,18 @@ type CreateTransactionRequest struct {
 }
 
 func (a Activities) CreateTransaction(ctx context.Context, request CreateTransactionRequest) (*shared.CreateTransactionResponse, error) {
+	versionsResponse, err := a.client.GetVersions(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if versionsResponse.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code %d when getting versions", versionsResponse.StatusCode)
+	}
+
+	version := collectionutils.Filter(versionsResponse.GetVersionsResponse.Versions, func(version shared.Version) bool {
+		return version.Name == "ledger"
+	})[0].Version
+
 	response, err := a.client.Transactions.
 		CreateTransaction(
 			ctx,
@@ -25,22 +41,32 @@ func (a Activities) CreateTransaction(ctx context.Context, request CreateTransac
 				Ledger:          request.Ledger,
 			},
 		)
-	if err != nil {
-		return nil, err
-	}
-
-	switch response.StatusCode {
-	case http.StatusOK:
-		return response.CreateTransactionResponse, nil
-	default:
-		if response.ErrorResponse != nil {
-			return nil, temporal.NewApplicationError(
-				response.ErrorResponse.ErrorMessage,
-				string(response.ErrorResponse.ErrorCode),
-				response.ErrorResponse.Details)
+	if semver.IsValid(version) && semver.Compare(version, "v2.0.0") < 0 {
+		baseResponse := api.BaseResponse[[]shared.Transaction]{}
+		if err := json.NewDecoder(response.RawResponse.Body).Decode(&baseResponse); err != nil {
+			return nil, err
+		}
+		return &shared.CreateTransactionResponse{
+			Data: (*baseResponse.Data)[0],
+		}, nil
+	} else {
+		if err != nil {
+			return nil, err
 		}
 
-		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+		switch response.StatusCode {
+		case http.StatusOK:
+			return response.CreateTransactionResponse, nil
+		default:
+			if response.ErrorResponse != nil {
+				return nil, temporal.NewApplicationError(
+					response.ErrorResponse.ErrorMessage,
+					string(response.ErrorResponse.ErrorCode),
+					response.ErrorResponse.Details)
+			}
+
+			return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+		}
 	}
 }
 
