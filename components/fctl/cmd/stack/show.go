a0cd8821f00171c4e8caa1aeacbcc77b6898e9eb
David Ragot <david@formance.com>
2023-06-13 17:20:40 +0200

    wip: shared store working with `fctl stack show --outputjson`

--- components/fctl/cmd/stack/show.go
+++ components/fctl/cmd/stack/show.go
@@ -7,83 +7,105 @@ import (
 	"github.com/formancehq/fctl/cmd/stack/internal"
 	"github.com/formancehq/fctl/membershipclient"
 	fctl "github.com/formancehq/fctl/pkg"
+	"github.com/formancehq/formance-sdk-go/pkg/models/shared"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 var errStackNotFound = errors.New("stack not found")
 
+const stackNameFlag = "name"
+
 func NewShowCommand() *cobra.Command {
-	const stackNameFlag = "name"
 
 	return fctl.NewMembershipCommand("show (<stack-id> | --name=<stack-name>)",
 		fctl.WithAliases("s", "sh"),
 		fctl.WithShortDescription("Show stack"),
 		fctl.WithArgs(cobra.MaximumNArgs(1)),
+		fctl.WithOutputFlag(),
 		fctl.WithStringFlag(stackNameFlag, "", ""),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-			organization, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return errors.Wrap(err, "searching default organization")
-			}
+		fctl.WithRunE(showCommand),
+		fctl.WrapOutputPostRunE(viewStackInformation),
+	)
+}
 
-			apiClient, err := fctl.NewMembershipClient(cmd, cfg)
-			if err != nil {
-				return err
-			}
+func showCommand(cmd *cobra.Command, args []string) error {
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return err
+	}
+	organization, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return errors.Wrap(err, "searching default organization")
+	}
 
-			var stack *membershipclient.Stack
-			if len(args) == 1 {
-				if fctl.GetString(cmd, stackNameFlag) != "" {
-					return errors.New("need either an id of a name specified using --name flag")
-				}
-				stackResponse, httpResponse, err := apiClient.DefaultApi.ReadStack(cmd.Context(), organization, args[0]).Execute()
-				if err != nil {
-					if httpResponse.StatusCode == http.StatusNotFound {
-						return errStackNotFound
-					}
-					return errors.Wrap(err, "listing stacks")
-				}
-				stack = stackResponse.Data
-			} else {
-				if fctl.GetString(cmd, stackNameFlag) == "" {
-					return errors.New("need either an id of a name specified using --name flag")
-				}
-				stacksResponse, _, err := apiClient.DefaultApi.ListStacks(cmd.Context(), organization).Execute()
-				if err != nil {
-					return errors.Wrap(err, "listing stacks")
-				}
-				for _, s := range stacksResponse.Data {
-					if s.Name == fctl.GetString(cmd, stackNameFlag) {
-						stack = &s
-						break
-					}
-				}
-			}
+	apiClient, err := fctl.NewMembershipClient(cmd, cfg)
+	if err != nil {
+		return err
+	}
 
-			if stack == nil {
+	var stack *membershipclient.Stack
+	if len(args) == 1 {
+		if fctl.GetString(cmd, stackNameFlag) != "" {
+			return errors.New("need either an id of a name specified using --name flag")
+		}
+		stackResponse, httpResponse, err := apiClient.DefaultApi.ReadStack(cmd.Context(), organization, args[0]).Execute()
+		if err != nil {
+			if httpResponse.StatusCode == http.StatusNotFound {
 				return errStackNotFound
 			}
-
-			stackClient, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return err
+			return errors.Wrap(err, "listing stacks")
+		}
+		stack = stackResponse.Data
+	} else {
+		if fctl.GetString(cmd, stackNameFlag) == "" {
+			return errors.New("need either an id of a name specified using --name flag")
+		}
+		stacksResponse, _, err := apiClient.DefaultApi.ListStacks(cmd.Context(), organization).Execute()
+		if err != nil {
+			return errors.Wrap(err, "listing stacks")
+		}
+		for _, s := range stacksResponse.Data {
+			if s.Name == fctl.GetString(cmd, stackNameFlag) {
+				stack = &s
+				break
 			}
+		}
+	}
 
-			versions, err := stackClient.GetVersions(cmd.Context())
-			if err != nil {
-				return err
-			}
+	if stack == nil {
+		return errStackNotFound
+	}
 
-			if versions.StatusCode != http.StatusOK {
-				return fmt.Errorf("unexpected status code %d when reading versions", versions.StatusCode)
-			}
+	stackClient, err := fctl.NewStackClient(cmd, cfg, stack)
+	if err != nil {
+		return err
+	}
 
-			return internal.PrintStackInformation(cmd.OutOrStdout(), fctl.GetCurrentProfile(cmd, cfg), stack, versions.GetVersionsResponse)
-		}),
-	)
+	versions, err := stackClient.GetVersions(cmd.Context())
+	if err != nil {
+		return err
+	}
+
+	if versions.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code %d when reading versions", versions.StatusCode)
+	}
+
+	fctl.SetSharedData(&StackInformation{
+		Stack:    stack,
+		Versions: versions.GetVersionsResponse,
+	}, nil, cfg)
+
+	return nil
+
+}
+
+type StackInformation struct {
+	Stack    *membershipclient.Stack     `json:"stack"`
+	Versions *shared.GetVersionsResponse `json:"versions"`
+}
+
+func viewStackInformation(cmd *cobra.Command, args []string) error {
+	data := fctl.GetSharedData().(*StackInformation)
+	return internal.PrintStackInformation(cmd.OutOrStdout(), fctl.GetCurrentProfile(cmd, fctl.GetSharedConfig()), data.Stack, data.Versions)
 }
