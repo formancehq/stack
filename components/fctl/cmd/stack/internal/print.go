b44c875e805cbc4d7db4a25d5cbb648eb0340986
David Ragot <david@formance.com>
2023-06-13 17:09:35 +0200

    wip: refacto `PrintStackInformation`

--- components/fctl/cmd/stack/internal/print.go
+++ components/fctl/cmd/stack/internal/print.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"io"
+	"net/url"
 
 	"github.com/formancehq/fctl/membershipclient"
 	fctl "github.com/formancehq/fctl/pkg"
@@ -12,37 +13,64 @@ import (
 )
 
 func PrintStackInformation(out io.Writer, profile *fctl.Profile, stack *membershipclient.Stack, versions *shared.GetVersionsResponse) error {
-	baseUrlStr := profile.ServicesBaseUrl(stack).String()
+	baseUrlStr := profile.ServicesBaseUrl(stack)
+
+	err := printInformation(out, stack)
+
+	if err != nil {
+		return err
+	}
+
+	err = printVersion(out, baseUrlStr, versions, stack)
+
+	if err != nil {
+		return err
+	}
+
+	err = printMetadata(out, stack)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func printInformation(out io.Writer, stack *membershipclient.Stack) error {
 
 	fctl.Section.WithWriter(out).Println("Information")
 	tableData := pterm.TableData{}
 	tableData = append(tableData, []string{pterm.LightCyan("ID"), stack.Id, ""})
 	tableData = append(tableData, []string{pterm.LightCyan("Name"), stack.Name, ""})
 	tableData = append(tableData, []string{pterm.LightCyan("Region"), stack.RegionID, ""})
-	if err := pterm.DefaultTable.
+	return pterm.DefaultTable.
 		WithWriter(out).
 		WithData(tableData).
-		Render(); err != nil {
-		return err
-	}
+		Render()
+}
 
+func printVersion(out io.Writer, url *url.URL, versions *shared.GetVersionsResponse, stack *membershipclient.Stack) error {
 	fctl.Println()
 	fctl.Section.WithWriter(out).Println("Versions")
-	tableData = pterm.TableData{}
+
+	tableData := pterm.TableData{}
+
 	for _, service := range versions.Versions {
 		tableData = append(tableData, []string{pterm.LightCyan(strcase.ToCamel(service.Name)), service.Version,
-			fmt.Sprintf("%s/api/%s", baseUrlStr, service.Name)})
-	}
-	if err := pterm.DefaultTable.
-		WithWriter(out).
-		WithData(tableData).
-		Render(); err != nil {
-		return err
+			fmt.Sprintf("%s/api/%s", url.String(), service.Name)})
 	}
 
+	return pterm.DefaultTable.
+		WithWriter(out).
+		WithData(tableData).
+		Render()
+}
+
+func printMetadata(out io.Writer, stack *membershipclient.Stack) error {
 	fctl.Println()
 	fctl.Section.WithWriter(out).Println("Metadata")
-	tableData = pterm.TableData{}
+
+	tableData := pterm.TableData{}
+
 	for k, v := range stack.Metadata {
 		tableData = append(tableData, []string{pterm.LightCyan(k), v})
 	}
