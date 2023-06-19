865da4c8822464aacbb6769d8f9dcd3b31a3be75
David Ragot <david@formance.com>
2023-06-19 18:47:23 +0200

    feat: convert all webhook command to json

--- components/fctl/cmd/webhooks/activate.go
+++ components/fctl/cmd/webhooks/activate.go
@@ -10,55 +10,73 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func RunActivateWebhookfunc(cmd *cobra.Command, args []string) error {
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return errors.Wrap(err, "fctl.GetConfig")
+	}
+
+	organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return err
+	}
+
+	stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
+	if err != nil {
+		return err
+	}
+
+	if !fctl.CheckStackApprobation(cmd, stack, "You are bout to activate a webhook") {
+		return fctl.ErrMissingApproval
+	}
+
+	client, err := fctl.NewStackClient(cmd, cfg, stack)
+	if err != nil {
+		return errors.Wrap(err, "creating stack client")
+	}
+
+	request := operations.ActivateConfigRequest{
+		ID: args[0],
+	}
+	response, err := client.Webhooks.ActivateConfig(cmd.Context(), request)
+	if err != nil {
+		return errors.Wrap(err, "activating config")
+	}
+
+	if response.ErrorResponse != nil {
+		return fmt.Errorf("%s: %s", response.ErrorResponse.ErrorCode, response.ErrorResponse.ErrorMessage)
+	}
+
+	if response.StatusCode >= 300 {
+		return fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	output := &OutputActivateWebhook{
+		Success: response.ConfigResponse.Data.Active,
+	}
+
+	fctl.SetSharedData(output, nil, nil, nil)
+
+	return nil
+}
+
+type OutputActivateWebhook struct {
+	Success bool `json:"success"`
+}
+
+func DisplayOutputActivateWebhook(cmd *cobra.Command, args []string) error {
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Config activated successfully")
+
+	return nil
+}
+
 func NewActivateCommand() *cobra.Command {
 	return fctl.NewCommand("activate <config-id>",
 		fctl.WithShortDescription("Activate one config"),
 		fctl.WithAliases("ac", "a"),
 		fctl.WithConfirmFlag(),
 		fctl.WithArgs(cobra.ExactArgs(1)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return errors.Wrap(err, "fctl.GetConfig")
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			if !fctl.CheckStackApprobation(cmd, stack, "You are bout to activate a webhook") {
-				return fctl.ErrMissingApproval
-			}
-
-			client, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return errors.Wrap(err, "creating stack client")
-			}
-
-			request := operations.ActivateConfigRequest{
-				ID: args[0],
-			}
-			response, err := client.Webhooks.ActivateConfig(cmd.Context(), request)
-			if err != nil {
-				return errors.Wrap(err, "activating config")
-			}
-
-			if response.ErrorResponse != nil {
-				return fmt.Errorf("%s: %s", response.ErrorResponse.ErrorCode, response.ErrorResponse.ErrorMessage)
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Config activated successfully")
-			return nil
-		}),
+		fctl.WithRunE(RunActivateWebhookfunc),
+		fctl.WrapOutputPostRunE(DisplayOutputActivateWebhook),
 	)
 }
