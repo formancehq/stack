32ed7959194ba533389a2bd4fd2a32c365c1a469
David Ragot <david@formance.com>
2023-06-19 15:47:14 +0200

    feat: `fctl ui --output=json`

--- components/fctl/cmd/ui.go
+++ components/fctl/cmd/ui.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os/exec"
 	"runtime"
 
@@ -28,31 +29,62 @@ func openUrl(url string) error {
 	return exec.Command(cmd, args...).Start()
 }
 
+func RunUiCommand(cmd *cobra.Command, args []string) error {
+
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return err
+	}
+
+	organization, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return err
+	}
+
+	stack, err := fctl.ResolveStack(cmd, cfg, organization)
+	if err != nil {
+		return err
+	}
+
+	profile := fctl.GetCurrentProfile(cmd, cfg)
+	stackUrl := profile.ServicesBaseUrl(stack)
+
+	ui := UiOutput{
+		UIUrl:        stackUrl.String(),
+		FoundBrowser: false,
+	}
+
+	if err := openUrl(ui.UIUrl); err != nil {
+		ui.FoundBrowser = true
+	}
+
+	fctl.SetSharedData(ui, profile, cfg, nil)
+	return errors.Wrap(err, "opening ui")
+
+}
+
+type UiOutput struct {
+	UIUrl        string `json:"stack_url"`
+	FoundBrowser bool   `json:"browser_found"`
+}
+
+func DisplayOutput(cmd *cobra.Command, args []string) error {
+
+	ui, ok := fctl.GetSharedData().(UiOutput)
+	if !ok {
+		return errors.New("invalid output")
+	}
+
+	fmt.Println("Opening url: ", ui.UIUrl)
+
+	return nil
+}
+
 func NewUICommand() *cobra.Command {
 	return fctl.NewStackCommand("ui",
 		fctl.WithShortDescription("Open UI"),
 		fctl.WithArgs(cobra.ExactArgs(0)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			organization, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organization)
-			if err != nil {
-				return err
-			}
-
-			profile := fctl.GetCurrentProfile(cmd, cfg)
-			stackUrl := profile.ServicesBaseUrl(stack)
-
-			return errors.Wrapf(openUrl(stackUrl.String()), "opening url: %s", stackUrl.String())
-		}),
+		fctl.WithRunE(RunUiCommand),
+		fctl.WrapOutputPostRunE(DisplayOutput),
 	)
 }
