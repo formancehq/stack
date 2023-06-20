a359d55a3057cbe9c779e2bc7ffd04aa36aab469
David Ragot <david@formance.com>
2023-06-20 17:56:57 +0200

    feat: interface `ProfilesCommands` with Controller && Renderable

--- components/fctl/cmd/profiles/list.go
+++ components/fctl/cmd/profiles/list.go
@@ -6,32 +6,69 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ProfilesListStore struct {
+	Profiles []string `json:"profiles"`
+}
+type ProfilesListController struct {
+	store              *ProfilesListStore
+	currentProfileName string
+}
+
+var _ fctl.Controller[*ProfilesListStore] = (*ProfilesListController)(nil)
+
+func NewDefaultProfilesListStore() *ProfilesListStore {
+	return &ProfilesListStore{
+		Profiles: []string{},
+	}
+}
+
+func NewProfilesListController() *ProfilesListController {
+	return &ProfilesListController{
+		store:              NewDefaultProfilesListStore(),
+		currentProfileName: "",
+	}
+}
+
+func (c *ProfilesListController) GetStore() *ProfilesListStore {
+	return c.store
+}
+
+func (c *ProfilesListController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	c.store.Profiles = fctl.MapKeys(cfg.GetProfiles())
+
+	c.currentProfileName = fctl.GetCurrentProfileName(cmd, cfg)
+
+	return c, nil
+
+}
+
+func (c *ProfilesListController) Render(cmd *cobra.Command, args []string) error {
+	tableData := fctl.Map(c.store.Profiles, func(p string) []string {
+		isCurrent := "No"
+		if p == c.currentProfileName {
+			isCurrent = "Yes"
+		}
+		return []string{p, isCurrent}
+	})
+	tableData = fctl.Prepend(tableData, []string{"Name", "Active"})
+
+	pterm.DefaultTable.
+		WithHasHeader().
+		WithWriter(cmd.OutOrStdout()).
+		WithData(tableData).
+		Render()
+	return nil
+}
+
 func NewListCommand() *cobra.Command {
 	return fctl.NewCommand("list",
 		fctl.WithAliases("l"),
 		fctl.WithShortDescription("List profiles"),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			currentProfileName := fctl.GetCurrentProfileName(cmd, cfg)
-
-			profiles := fctl.MapKeys(cfg.GetProfiles())
-			tableData := fctl.Map(profiles, func(p string) []string {
-				isCurrent := "No"
-				if p == currentProfileName {
-					isCurrent = "Yes"
-				}
-				return []string{p, isCurrent}
-			})
-			tableData = fctl.Prepend(tableData, []string{"Name", "Active"})
-			return pterm.DefaultTable.
-				WithHasHeader().
-				WithWriter(cmd.OutOrStdout()).
-				WithData(tableData).
-				Render()
-		}))
+		fctl.WithController[*ProfilesListStore](NewProfilesListController()),
+	)
 }
