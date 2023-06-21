a7c8a0492fae4a6e052174f5ede1cc14bea4ad79
David Ragot <david@formance.com>
2023-06-21 18:00:10 +0200

    feat: convert all commands `fctl orchestration workflows XXX`

--- components/fctl/cmd/orchestration/workflows/list.go
+++ components/fctl/cmd/orchestration/workflows/list.go
@@ -11,76 +11,97 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type OrchestrationWorkflowsListStore struct {
+	Workflows []shared.Workflow `json:"workflows"`
+}
+type OrchestrationWorkflowsListController struct {
+	store *OrchestrationWorkflowsListStore
+}
+
+var _ fctl.Controller[*OrchestrationWorkflowsListStore] = (*OrchestrationWorkflowsListController)(nil)
+
+func NewDefaultOrchestrationWorkflowsListStore() *OrchestrationWorkflowsListStore {
+	return &OrchestrationWorkflowsListStore{}
+}
+
+func NewOrchestrationWorkflowsListController() *OrchestrationWorkflowsListController {
+	return &OrchestrationWorkflowsListController{
+		store: NewDefaultOrchestrationWorkflowsListStore(),
+	}
+}
+
 func NewListCommand() *cobra.Command {
 	return fctl.NewCommand("list",
 		fctl.WithShortDescription("List all workflows"),
 		fctl.WithAliases("ls", "l"),
 		fctl.WithArgs(cobra.ExactArgs(0)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return errors.Wrap(err, "retrieving config")
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			client, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return errors.Wrap(err, "creating stack client")
-			}
-
-			response, err := client.Orchestration.ListWorkflows(cmd.Context())
-			if err != nil {
-				return err
-			}
-
-			if response.Error != nil {
-				return fmt.Errorf("%s: %s", response.Error.ErrorCode, response.Error.ErrorMessage)
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			if len(response.ListWorkflowsResponse.Data) == 0 {
-				fctl.Println("No workflows found.")
-				return nil
-			}
-
-			if err := pterm.DefaultTable.
-				WithHasHeader(true).
-				WithWriter(cmd.OutOrStdout()).
-				WithData(
-					fctl.Prepend(
-						fctl.Map(response.ListWorkflowsResponse.Data,
-							func(src shared.Workflow) []string {
-								return []string{
-									src.ID,
-									func() string {
-										if src.Config.Name != nil {
-											return *src.Config.Name
-										}
-										return ""
-									}(),
-									src.CreatedAt.Format(time.RFC3339),
-									src.UpdatedAt.Format(time.RFC3339),
-								}
-							}),
-						[]string{"ID", "Name", "Created at", "Updated at"},
-					),
-				).Render(); err != nil {
-				return errors.Wrap(err, "rendering table")
-			}
-
-			return nil
-		}),
+		fctl.WithController[*OrchestrationWorkflowsListStore](NewOrchestrationWorkflowsListController()),
 	)
+}
+
+func (c *OrchestrationWorkflowsListController) GetStore() *OrchestrationWorkflowsListStore {
+	return c.store
+}
+
+func (c *OrchestrationWorkflowsListController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+
+	soc, err := fctl.GetStackOrganizationConfig(cmd)
+	if err != nil {
+		return nil, err
+	}
+	client, err := fctl.NewStackClient(cmd, soc.Config, soc.Stack)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating stack client")
+	}
+
+	response, err := client.Orchestration.ListWorkflows(cmd.Context())
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Error != nil {
+		return nil, fmt.Errorf("%s: %s", response.Error.ErrorCode, response.Error.ErrorMessage)
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.Workflows = response.ListWorkflowsResponse.Data
+
+	return c, nil
+}
+
+func (c *OrchestrationWorkflowsListController) Render(cmd *cobra.Command, args []string) error {
+	if len(c.store.Workflows) == 0 {
+		fctl.Println("No workflows found.")
+		return nil
+	}
+
+	if err := pterm.DefaultTable.
+		WithHasHeader(true).
+		WithWriter(cmd.OutOrStdout()).
+		WithData(
+			fctl.Prepend(
+				fctl.Map(c.store.Workflows,
+					func(src shared.Workflow) []string {
+						return []string{
+							src.ID,
+							func() string {
+								if src.Config.Name != nil {
+									return *src.Config.Name
+								}
+								return ""
+							}(),
+							src.CreatedAt.Format(time.RFC3339),
+							src.UpdatedAt.Format(time.RFC3339),
+						}
+					}),
+				[]string{"ID", "Name", "Created at", "Updated at"},
+			),
+		).Render(); err != nil {
+		return errors.Wrap(err, "rendering table")
+	}
+
+	return nil
 }
