d5a1b9df1ce47cb29fb9dfc2066eb23f0dc39999
David Ragot <david@formance.com>
2023-06-21 15:05:12 +0200

    feat: convert all command `fctl payments <XXX> --output=<plain|json>`

--- components/fctl/cmd/payments/connectors/install/wise.go
+++ components/fctl/cmd/payments/connectors/install/wise.go
@@ -12,52 +12,83 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type PaymentsConnectorsWiseStore struct {
+	Success       bool   `json:"success"`
+	ConnectorName string `json:"connectorName"`
+}
+type PaymentsConnectorsWiseController struct {
+	store                        *PaymentsConnectorsWiseStore
+	endpointFlag                 string
+	authorizationEndpointFlag    string
+	defaultEndpoint              string
+	defaultAuthorizationEndpoint string
+}
+
+var _ fctl.Controller[*PaymentsConnectorsWiseStore] = (*PaymentsConnectorsWiseController)(nil)
+
+func NewDefaultPaymentsConnectorsWiseStore() *PaymentsConnectorsWiseStore {
+	return &PaymentsConnectorsWiseStore{
+		Success: false,
+	}
+}
+
+func NewPaymentsConnectorsWiseController() *PaymentsConnectorsWiseController {
+	return &PaymentsConnectorsWiseController{
+		store:                        NewDefaultPaymentsConnectorsWiseStore(),
+		endpointFlag:                 "endpoint",
+		authorizationEndpointFlag:    "authorization-endpoint",
+		defaultEndpoint:              "https://sandbox.Wise.com",
+		defaultAuthorizationEndpoint: "https://authorizationsandbox.Wiseconnect.com",
+	}
+}
+
 func NewWiseCommand() *cobra.Command {
+	c := NewPaymentsConnectorsWiseController()
 	return fctl.NewCommand(internal.WiseConnector+" <api-key>",
 		fctl.WithShortDescription("Install a Wise connector"),
 		fctl.WithArgs(cobra.ExactArgs(1)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			if !fctl.CheckStackApprobation(cmd, stack, "You are about to install connector '%s'", internal.WiseConnector) {
-				return fctl.ErrMissingApproval
-			}
-
-			paymentsClient, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return err
-			}
-
-			response, err := paymentsClient.Payments.InstallConnector(cmd.Context(), operations.InstallConnectorRequest{
-				RequestBody: shared.WiseConfig{
-					APIKey: args[1],
-				},
-				Connector: shared.ConnectorWise,
-			})
-			if err != nil {
-				return errors.Wrap(err, "installing connector")
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Connector installed!")
-
-			return nil
-		}),
+		fctl.WithController[*PaymentsConnectorsWiseStore](c),
 	)
+}
+
+func (c *PaymentsConnectorsWiseController) GetStore() *PaymentsConnectorsWiseStore {
+	return c.store
+}
+
+func (c *PaymentsConnectorsWiseController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+	soc, err := fctl.GetStackOrganizationConfigApprobation(cmd, "You are about to install connector '%s'", internal.WiseConnector)
+	if err != nil {
+		return nil, fctl.ErrMissingApproval
+	}
+
+	paymentsClient, err := fctl.NewStackClient(cmd, soc.Config, soc.Stack)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := paymentsClient.Payments.InstallConnector(cmd.Context(), operations.InstallConnectorRequest{
+		RequestBody: shared.WiseConfig{
+			APIKey: args[1],
+		},
+		Connector: shared.ConnectorWise,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "installing connector")
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.Success = true
+	c.store.ConnectorName = internal.WiseConnector
+
+	return c, nil
+}
+
+func (c *PaymentsConnectorsWiseController) Render(cmd *cobra.Command, args []string) error {
+
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Connector '%s' installed!", c.store.ConnectorName)
+
+	return nil
 }
--- components/fctl/cmd/payments/list.go
+++ components/fctl/cmd/payments/list.go
@@ -11,67 +11,99 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type PaymentsListStore struct {
+	Cursor *shared.PaymentsCursorCursor `json:"cursor"`
+}
+type PaymentsListController struct {
+	store *PaymentsListStore
+}
+
+var _ fctl.Controller[*PaymentsListStore] = (*PaymentsListController)(nil)
+
+func NewDefaultPaymentsListStore() *PaymentsListStore {
+	return &PaymentsListStore{
+		Cursor: &shared.PaymentsCursorCursor{},
+	}
+}
+
+func NewPaymentsListController() *PaymentsListController {
+	return &PaymentsListController{
+		store: NewDefaultPaymentsListStore(),
+	}
+}
+
+func (c *PaymentsListController) GetStore() *PaymentsListStore {
+	return c.store
+}
+
+func (c *PaymentsListController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := fctl.NewStackClient(cmd, cfg, stack)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Payments.ListPayments(
+		cmd.Context(),
+		operations.ListPaymentsRequest{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.Cursor = &response.PaymentsCursor.Cursor
+
+	return c, nil
+}
+
+func (c *PaymentsListController) Render(cmd *cobra.Command, args []string) error {
+	tableData := fctl.Map(c.store.Cursor.Data, func(payment shared.Payment) []string {
+		return []string{
+			payment.ID,
+			string(payment.Type),
+			fmt.Sprint(payment.InitialAmount),
+			payment.Asset,
+			string(payment.Status),
+			string(payment.Scheme),
+			payment.Reference,
+			payment.AccountID,
+			string(payment.Provider),
+			payment.CreatedAt.Format(time.RFC3339),
+		}
+	})
+	tableData = fctl.Prepend(tableData, []string{"ID", "Type", "Amount", "Asset", "Status",
+		"Scheme", "Reference", "Account ID", "Provider", "Created at"})
+	return pterm.DefaultTable.
+		WithHasHeader().
+		WithWriter(cmd.OutOrStdout()).
+		WithData(tableData).
+		Render()
+}
+
 func NewListPaymentsCommand() *cobra.Command {
 	return fctl.NewCommand("list",
 		fctl.WithAliases("ls"),
 		fctl.WithArgs(cobra.ExactArgs(0)),
 		fctl.WithShortDescription("List payments"),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			client, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return err
-			}
-
-			response, err := client.Payments.ListPayments(
-				cmd.Context(),
-				operations.ListPaymentsRequest{},
-			)
-			if err != nil {
-				return err
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			paymentsCursor := response.PaymentsCursor
-			tableData := fctl.Map(paymentsCursor.Cursor.Data, func(payment shared.Payment) []string {
-				return []string{
-					payment.ID,
-					string(payment.Type),
-					fmt.Sprint(payment.InitialAmount),
-					payment.Asset,
-					string(payment.Status),
-					string(payment.Scheme),
-					payment.Reference,
-					payment.AccountID,
-					string(payment.Provider),
-					payment.CreatedAt.Format(time.RFC3339),
-				}
-			})
-			tableData = fctl.Prepend(tableData, []string{"ID", "Type", "Amount", "Asset", "Status",
-				"Scheme", "Reference", "Account ID", "Provider", "Created at"})
-			return pterm.DefaultTable.
-				WithHasHeader().
-				WithWriter(cmd.OutOrStdout()).
-				WithData(tableData).
-				Render()
-		}),
+		fctl.WithController[*PaymentsListStore](NewPaymentsListController()),
 	)
 }
