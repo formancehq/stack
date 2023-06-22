038bb309daf3da5fe7989ccf41960d53aa2a33af
Maxence Maireaux <maxence@maireaux.fr>
2023-06-22 14:42:45 +0200

    feat: Upgrade moon

--- nix/moon/pkgs/moon/default.nix
+++ nix/moon/pkgs/moon/default.nix
@@ -2,25 +2,25 @@
 { system ? builtins.currentSystem, pkgs, lib, fetchurl, installShellFiles }:
 let
   shaMap = {
-    x86_64-linux = "c0c8fdefd9f5f53671308431c198973133c53e1543399e68855e5988f2d0566a";
-    aarch64-linux = "47982a157d9b92d5c699cf078d2756d80d6703a4771b0cd8689be8f388551ad3";
-    x86_64-darwin = "41a1c9f87b7843c8945556f0c73b0a4ae3b2bc8d4a9a1535a6c3f2330819cb62";
-    aarch64-darwin = "94de430230cd3687b5444f2fbcfc548440432def7ce33c9c23dde0f1b71a2ce7";
+    x86_64-linux = "de5e783840fedeed58d00601a00db029db8a63ff46277928bb04ff07b310eadd";
+    aarch64-linux = "cecbd6e85f1baa7e79908710ea1a309e3db7cbbc7f1e3f69a417ee399de98a92";
+    x86_64-darwin = "2abc5d1db5a4b4fa94f0e2773802a559acdfb5017e5ba2a3edfce538d9b542e8";
+    aarch64-darwin = "c70fd2e4fb9c062173895b1da6105301771f190b54d28b152030457104ca555e";
   };
 
   urlMap = {
     x86_64-linux =
-      "https://github.com/moonrepo/moon/releases/download/v1.6.1/moon-x86_64-unknown-linux-gnu";
+      "https://github.com/moonrepo/moon/releases/download/v1.8.3/moon-x86_64-unknown-linux-gnu";
     aarch64-linux =
-      "https://github.com/moonrepo/moon/releases/download/v1.6.1/moon-aarch64-unknown-linux-gnu";
+      "https://github.com/moonrepo/moon/releases/download/v1.8.3/moon-aarch64-unknown-linux-gnu";
     x86_64-darwin =
-      "https://github.com/moonrepo/moon/releases/download/v1.6.1/moon-x86_64-apple-darwin";
+      "https://github.com/moonrepo/moon/releases/download/v1.8.3/moon-x86_64-apple-darwin";
     aarch64-darwin =
-      "https://github.com/moonrepo/moon/releases/download/v1.6.1/moon-aarch64-apple-darwin";
+      "https://github.com/moonrepo/moon/releases/download/v1.8.3/moon-aarch64-apple-darwin";
   };
 in pkgs.stdenv.mkDerivation {
   pname = "moon";
-  version = "1.6.1";
+  version = "1.8.3";
   src = fetchurl {
     url = urlMap.${system};
     sha256 = shaMap.${system};
