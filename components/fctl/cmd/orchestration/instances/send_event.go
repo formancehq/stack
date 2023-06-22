dd087f29af8808dc010a10feb2f177177a14e9aa
David Ragot <david@formance.com>
2023-06-22 17:43:43 +0200

    feat: convert send_event to json

--- components/fctl/cmd/orchestration/instances/send_event.go
+++ components/fctl/cmd/orchestration/instances/send_event.go
@@ -10,52 +10,80 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type InstancesSendEventStore struct {
+	Success    bool   `json:"success"`
+	InstanceID string `json:"instance_id"`
+	Event      string `json:"event"`
+}
+type InstancesSendEventController struct {
+	store *InstancesSendEventStore
+}
+
+var _ fctl.Controller[*InstancesSendEventStore] = (*InstancesSendEventController)(nil)
+
+func NewDefaultInstancesSendEventStore() *InstancesSendEventStore {
+	return &InstancesSendEventStore{
+		Success:    false,
+		InstanceID: "",
+		Event:      "",
+	}
+}
+
+func NewInstancesSendEventController() *InstancesSendEventController {
+	return &InstancesSendEventController{
+		store: NewDefaultInstancesSendEventStore(),
+	}
+}
+
 func NewSendEventCommand() *cobra.Command {
 	return fctl.NewCommand("send-event <instance-id> <event>",
 		fctl.WithShortDescription("Send an event to an instance"),
 		fctl.WithArgs(cobra.ExactArgs(2)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return errors.Wrap(err, "retrieving config")
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			client, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return errors.Wrap(err, "creating stack client")
-			}
-
-			response, err := client.Orchestration.SendEvent(cmd.Context(), operations.SendEventRequest{
-				RequestBody: &operations.SendEventRequestBody{
-					Name: args[1],
-				},
-				InstanceID: args[0],
-			})
-			if err != nil {
-				return err
-			}
-
-			if response.Error != nil {
-				return fmt.Errorf("%s: %s", response.Error.ErrorCode, response.Error.ErrorMessage)
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Event '%s' sent", args[1])
-
-			return nil
-		}),
+		fctl.WithController[*InstancesSendEventStore](NewInstancesSendEventController()),
 	)
+}
+
+func (c *InstancesSendEventController) GetStore() *InstancesSendEventStore {
+	return c.store
+}
+
+func (c *InstancesSendEventController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+	soc, err := fctl.GetStackOrganizationConfig(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := fctl.NewStackClient(cmd, soc.Config, soc.Stack)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating stack client")
+	}
+	response, err := client.Orchestration.SendEvent(cmd.Context(), operations.SendEventRequest{
+		RequestBody: &operations.SendEventRequestBody{
+			Name: args[1],
+		},
+		InstanceID: args[0],
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Error != nil {
+		return nil, fmt.Errorf("%s: %s", response.Error.ErrorCode, response.Error.ErrorMessage)
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.Success = true
+	c.store.InstanceID = args[0]
+	c.store.Event = args[1]
+
+	return c, nil
+}
+
+func (c *InstancesSendEventController) Render(cmd *cobra.Command, args []string) error {
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Event '%s' sent", args[1])
+	return nil
 }
