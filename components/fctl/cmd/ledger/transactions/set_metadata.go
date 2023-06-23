8e21875028f33e86ebae2457824742f2f5acb03d
David Ragot <david@formance.com>
2023-06-23 10:05:37 +0200

    wip: SetMetaData working

--- components/fctl/cmd/ledger/transactions/set_metadata.go
+++ components/fctl/cmd/ledger/transactions/set_metadata.go
@@ -10,6 +10,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type SetMetadataStore struct {
+	Success bool `json:"success"`
+}
+type SetMetadataController struct {
+	store *SetMetadataStore
+}
+
+var _ fctl.Controller[*SetMetadataStore] = (*SetMetadataController)(nil)
+
+func NewDefaultSetMetadataStore() *SetMetadataStore {
+	return &SetMetadataStore{}
+}
+
+func NewSetMetadataController() *SetMetadataController {
+	return &SetMetadataController{
+		store: NewDefaultSetMetadataStore(),
+	}
+}
+
 func NewSetMetadataCommand() *cobra.Command {
 	return fctl.NewCommand("set-metadata <transaction-id> [<key>=<value>...]",
 		fctl.WithShortDescription("Set metadata on transaction"),
@@ -17,63 +36,75 @@ func NewSetMetadataCommand() *cobra.Command {
 		fctl.WithConfirmFlag(),
 		fctl.WithValidArgs("last"),
 		fctl.WithArgs(cobra.MinimumNArgs(2)),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-
-			metadata, err := fctl.ParseMetadata(args[1:])
-			if err != nil {
-				return err
-			}
-
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			ledgerClient, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return err
-			}
-
-			transactionID, err := internal.TransactionIDOrLastN(cmd.Context(), ledgerClient,
-				fctl.GetString(cmd, internal.LedgerFlag), args[0])
-			if err != nil {
-				return err
-			}
-
-			if !fctl.CheckStackApprobation(cmd, stack, "You are about to set a metadata on transaction %d", transactionID) {
-				return fctl.ErrMissingApproval
-			}
-
-			request := operations.AddMetadataOnTransactionRequest{
-				Ledger:      fctl.GetString(cmd, internal.LedgerFlag),
-				Txid:        transactionID,
-				RequestBody: metadata,
-			}
-			response, err := ledgerClient.Ledger.AddMetadataOnTransaction(cmd.Context(), request)
-			if err != nil {
-				return err
-			}
-
-			if response.ErrorResponse != nil {
-				return fmt.Errorf("%s: %s", response.ErrorResponse.ErrorCode, response.ErrorResponse.ErrorMessage)
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Metadata added!")
-			return nil
-		}),
+		fctl.WithController[*SetMetadataStore](NewSetMetadataController()),
 	)
+}
+
+func (c *SetMetadataController) GetStore() *SetMetadataStore {
+	return c.store
+}
+
+func (c *SetMetadataController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+
+	metadata, err := fctl.ParseMetadata(args[1:])
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
+	if err != nil {
+		return nil, err
+	}
+
+	ledgerClient, err := fctl.NewStackClient(cmd, cfg, stack)
+	if err != nil {
+		return nil, err
+	}
+
+	transactionID, err := internal.TransactionIDOrLastN(cmd.Context(), ledgerClient,
+		fctl.GetString(cmd, internal.LedgerFlag), args[0])
+	if err != nil {
+		return nil, err
+	}
+
+	if !fctl.CheckStackApprobation(cmd, stack, "You are about to set a metadata on transaction %d", transactionID) {
+		return nil, fctl.ErrMissingApproval
+	}
+
+	request := operations.AddMetadataOnTransactionRequest{
+		Ledger:      fctl.GetString(cmd, internal.LedgerFlag),
+		Txid:        transactionID,
+		RequestBody: metadata,
+	}
+	response, err := ledgerClient.Ledger.AddMetadataOnTransaction(cmd.Context(), request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.ErrorResponse != nil {
+		return nil, fmt.Errorf("%s: %s", response.ErrorResponse.ErrorCode, response.ErrorResponse.ErrorMessage)
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.Success = response.StatusCode == 204
+	return c, nil
+}
+
+// TODO: This need to use the ui.NewListModel
+func (c *SetMetadataController) Render(cmd *cobra.Command, args []string) error {
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Metadata added!")
+	return nil
 }
