94762a448c0d7022654857e4240227392ef99e13
David Ragot <david@formance.com>
2023-06-23 12:28:24 +0200

    wip: convert create to json

--- components/fctl/cmd/wallets/create.go
+++ components/fctl/cmd/wallets/create.go
@@ -10,66 +10,98 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type CreateStore struct {
+	WalletID string `json:"wallet_id"`
+}
+type CreateController struct {
+	store        *CreateStore
+	metadataFlag string
+}
+
+var _ fctl.Controller[*CreateStore] = (*CreateController)(nil)
+
+func NewDefaultCreateStore() *CreateStore {
+	return &CreateStore{}
+}
+
+func NewCreateController() *CreateController {
+	return &CreateController{
+		store:        NewDefaultCreateStore(),
+		metadataFlag: "metadata",
+	}
+}
+
 func NewCreateCommand() *cobra.Command {
-	const (
-		metadataFlag = "metadata"
-	)
+	c := NewCreateController()
 	return fctl.NewCommand("create <name>",
 		fctl.WithShortDescription("Create a new wallet"),
 		fctl.WithAliases("cr"),
 		fctl.WithConfirmFlag(),
 		fctl.WithArgs(cobra.ExactArgs(1)),
-		fctl.WithStringSliceFlag(metadataFlag, []string{""}, "Metadata to use"),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return errors.Wrap(err, "retrieving config")
-			}
-
-			organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
-			if err != nil {
-				return err
-			}
-
-			if !fctl.CheckStackApprobation(cmd, stack, "You are about to create a wallet") {
-				return fctl.ErrMissingApproval
-			}
-
-			client, err := fctl.NewStackClient(cmd, cfg, stack)
-			if err != nil {
-				return errors.Wrap(err, "creating stack client")
-			}
-
-			metadata, err := fctl.ParseMetadata(fctl.GetStringSlice(cmd, metadataFlag))
-			if err != nil {
-				return err
-			}
-
-			request := shared.CreateWalletRequest{
-				Name:     args[0],
-				Metadata: metadata,
-			}
-			response, err := client.Wallets.CreateWallet(cmd.Context(), request)
-			if err != nil {
-				return errors.Wrap(err, "creating wallet")
-			}
-
-			if response.WalletsErrorResponse != nil {
-				return fmt.Errorf("%s: %s", response.WalletsErrorResponse.ErrorCode, response.WalletsErrorResponse.ErrorMessage)
-			}
-
-			if response.StatusCode >= 300 {
-				return fmt.Errorf("unexpected status code: %d", response.StatusCode)
-			}
-
-			pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln(
-				"Wallet created successfully with ID: %s", response.CreateWalletResponse.Data.ID)
-			return nil
-		}),
+		fctl.WithStringSliceFlag(c.metadataFlag, []string{""}, "Metadata to use"),
+		fctl.WithController[*CreateStore](c),
 	)
+}
+
+func (c *CreateController) GetStore() *CreateStore {
+	return c.store
+}
+
+func (c *CreateController) Run(cmd *cobra.Command, args []string) (fctl.Renderable, error) {
+
+	cfg, err := fctl.GetConfig(cmd)
+	if err != nil {
+		return nil, errors.Wrap(err, "retrieving config")
+	}
+
+	organizationID, err := fctl.ResolveOrganizationID(cmd, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	stack, err := fctl.ResolveStack(cmd, cfg, organizationID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !fctl.CheckStackApprobation(cmd, stack, "You are about to create a wallet") {
+		return nil, fctl.ErrMissingApproval
+	}
+
+	client, err := fctl.NewStackClient(cmd, cfg, stack)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating stack client")
+	}
+
+	metadata, err := fctl.ParseMetadata(fctl.GetStringSlice(cmd, c.metadataFlag))
+	if err != nil {
+		return nil, err
+	}
+
+	request := shared.CreateWalletRequest{
+		Name:     args[0],
+		Metadata: metadata,
+	}
+	response, err := client.Wallets.CreateWallet(cmd.Context(), request)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating wallet")
+	}
+
+	if response.WalletsErrorResponse != nil {
+		return nil, fmt.Errorf("%s: %s", response.WalletsErrorResponse.ErrorCode, response.WalletsErrorResponse.ErrorMessage)
+	}
+
+	if response.StatusCode >= 300 {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	c.store.WalletID = response.CreateWalletResponse.Data.ID
+
+	return c, nil
+}
+
+func (c *CreateController) Render(cmd *cobra.Command, args []string) error {
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln(
+		"Wallet created successfully with ID: %s", c.store.WalletID)
+	return nil
 }
