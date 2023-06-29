31561ce549508788e8deec90d9cebfd118eb85cb
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-06-29 14:58:43 +0200

    feat: add long string template

--- components/payments/internal/app/connectors/configtemplate/types.go
+++ components/payments/internal/app/connectors/configtemplate/types.go
@@ -3,6 +3,7 @@ package configtemplate
 type Type string
 
 const (
+	TypeLongString              Type = "long string"
 	TypeString                  Type = "string"
 	TypeDurationNs              Type = "duration ns"
 	TypeDurationUnsignedInteger Type = "unsigned integer"
