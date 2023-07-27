c1e3cd5bd765b0ab41b155f0336849739e8af6ce
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-07-27 14:18:32 +0200

    add migration

--- components/payments/internal/app/storage/migrations.go
+++ components/payments/internal/app/storage/migrations.go
@@ -439,5 +439,22 @@ func registerMigrations(migrator *migrations.Migrator) {
 				return nil
 			},
 		},
+		migrations.Migration{
+			Up: func(tx bun.Tx) error {
+				// Since only one connector is inserting accounts,
+				// let's just delete the table, since connectors will be
+				// resetted. Delete it cascade, or we will have an error
+				_, err := tx.Exec(`
+					ALTER TABLE payments.adjustment ALTER COLUMN amount TYPE numeric;
+					ALTER TABLE payments.payment ALTER COLUMN amount TYPE numeric;
+					ALTER TABLE payments.transfers ALTER COLUMN amount TYPE numeric;
+				`)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
 	)
 }
