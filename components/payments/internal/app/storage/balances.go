7c91213432eaa96dab0fd9846abfc539f235b28c
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-08-02 10:00:44 +0200

    balances: always update the previos row

--- components/payments/internal/app/storage/balances.go
+++ components/payments/internal/app/storage/balances.go
@@ -15,13 +15,6 @@ func (s *Storage) InsertBalances(ctx context.Context, balances []*models.Balance
 		return nil
 	}
 
-	type insertedBalance struct {
-		CreatedAt time.Time    `bun:"created_at"`
-		AccountID string       `bun:"account_id"`
-		Asset     models.Asset `bun:"currency"`
-	}
-	insertedBalances := make([]insertedBalance, 0)
-
 	query := s.db.NewInsert().
 		Model((*models.Balance)(nil)).
 		With("cte1", s.db.NewValues(&balances)).
@@ -31,8 +24,7 @@ func (s *Storage) InsertBalances(ctx context.Context, balances []*models.Balance
 			"balance",
 			"currency",
 			"last_updated_at",
-		).
-		Returning("created_at, account_id, currency")
+		)
 	if checkIfAccountExists {
 		query = query.TableExpr(`
 		(SELECT *
@@ -48,41 +40,23 @@ func (s *Storage) InsertBalances(ctx context.Context, balances []*models.Balance
 		) data`)
 	}
 
-	err := query.Scan(ctx, &insertedBalances)
+	_, err := query.Exec(ctx)
 	if err != nil {
 		return e("failed to create balances", err)
 	}
 
-	if len(insertedBalances) != len(balances) {
-		balancesToUpdate := make([]*models.Balance, 0)
-		for i := range balances {
-			found := false
-			for _, insertedBalance := range insertedBalances {
-				if balances[i].AccountID.String() == insertedBalance.AccountID &&
-					balances[i].Asset == insertedBalance.Asset {
-					found = true
-					break
-				}
-			}
-			if !found {
-				balancesToUpdate = append(balancesToUpdate, balances[i])
-			}
-		}
-
-		if len(balancesToUpdate) > 0 {
-			_, err := s.db.NewUpdate().
-				Model((*models.Balance)(nil)).
-				With("cte1", s.db.NewValues(&balancesToUpdate)).
-				TableExpr(`
+	// Always update the previous row in order to keep the balance history consistent.
+	_, err = s.db.NewUpdate().
+		Model((*models.Balance)(nil)).
+		With("cte1", s.db.NewValues(&balances)).
+		TableExpr(`
 					(SELECT (SELECT created_at FROM accounts.balances WHERE last_updated_at < cte1.last_updated_at AND account_id = cte1.account_id AND currency = cte1.currency ORDER BY last_updated_at DESC LIMIT 1), cte1.account_id, cte1.currency, cte1.last_updated_at FROM cte1) data
 				`).
-				Set("last_updated_at = data.last_updated_at").
-				Where("balance.account_id = data.account_id AND balance.currency = data.currency AND balance.created_at = data.created_at").
-				Exec(ctx)
-			if err != nil {
-				return e("failed to update balances", err)
-			}
-		}
+		Set("last_updated_at = data.last_updated_at").
+		Where("balance.account_id = data.account_id AND balance.currency = data.currency AND balance.created_at = data.created_at").
+		Exec(ctx)
+	if err != nil {
+		return e("failed to update balances", err)
 	}
 
 	return nil
