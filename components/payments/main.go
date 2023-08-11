62a522f6c2423428691d449917a285b29e562b03
David Ragot <david.ragot14@gmail.com>
2023-08-11 11:58:34 +0200

    wip: remove latest go generate

--- components/payments/main.go
+++ components/payments/main.go
@@ -1,4 +1,3 @@
-//go:generate docker run --rm -w /local -v ${PWD}:/local openapitools/openapi-generator-cli:latest validate -i ./openapi.yaml
 package main
 
 import "github.com/formancehq/payments/cmd"
