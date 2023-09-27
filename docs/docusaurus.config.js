4948c2438363699d23affa99be57fcbc8e5c7028
Maxence Maireaux <maxence@maireaux.fr>
2023-09-27 17:58:31 +0200

    chore: sync witch main

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,38 +20,6 @@ const config = {
   organizationName: 'formancehq', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
-  stylesheets: [
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;300;400;500;600&display=swap',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600&display=swap',
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
-
-  // plugins: [
-  //   [
-  //     '@docusaurus/plugin-content-docs',
-  //     {
-  //       id: "product",
-  //       path: './product',
-  //       routeBasePath: '/product',
-  //       // sidebarPath: require.resolve('./product/flows/sidebars.js'),
-  //       remarkPlugins: [math],
-  //       rehypePlugins: [katex],
-  //       versions: {
-  //           current: {
-  //               label: 'v1.x',
-  //           }
-  //       }
-  //     },
-  //   ],
-  // ],
-
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -97,129 +65,129 @@ const config = {
             route: '/api/v2.x',
             id: 'api-v2',
           }
-      ],
+        ],
       }
     ],
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          autoCollapseCategories: true,
-        }
-      },
-      colorMode: {
-        defaultMode: 'light',
-        disableSwitch: true,
-      },
-      navbar: {
-        // style: 'light',
-        logo: {
-          alt: 'Formance Logo',
-          src: 'img/logo.svg',
-          href: '/',
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        docs: {
+          sidebar: {
+            autoCollapseCategories: true,
+          }
         },
-        items: [
-          {
-            type: 'docsVersionDropdown',
-            position: 'left',
+        colorMode: {
+          defaultMode: 'light',
+          disableSwitch: true,
+        },
+        navbar: {
+          // style: 'light',
+          logo: {
+            alt: 'Formance Logo',
+            src: 'img/logo.svg',
+            href: '/',
           },
-          {
-            label: 'Use-cases Library',
-            position: 'right',
-            href: 'https://www.formance.com/use-cases',
-          },
-          {
-            href: 'https://github.com/formancehq/stack',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            label: 'Go to Website',
-            position: 'right',
-            href: 'https://www.formance.com/',
-          },
-        ],
-      },
-      footer: {
-        style: 'light',
-        links: [
-          {
-            title: 'Documentation',
-            items: [
-              {
-                label: 'Ledger',
-                to: '/ledger',
-              },
-              {
-                label: 'Payments',
-                to: '/payments',
-              },
-              {
-                label: 'Wallets',
-                to: '/wallets',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Slack',
-                href: 'https://bit.ly/formance-slack',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/formancehq',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/formancehq/stack',
-              },
-              {
-                label: 'Cloud Status',
-                href: 'https://status.formance.com',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © 2021-2023 Formance, Inc`,
-      },
-      prism: {
-        theme: darkCodeTheme,
-      },
-      posthog: {
-        apiKey: 'phc_hRDv01yOHJNUM7l5SmXPUtSQUuNw4r5am9FtV83Z9om',
-        appUrl: 'https://app.posthog.com',  // optional
-        enableInDevelopment: false,  // optional
-      },
-      algolia: {
-        appId: 'IHGRMFJIIG',
-        apiKey: '7864304f16ea5f9d27b7a553c83ad17a',
-        indexName: 'numary',
+          items: [
+            {
+              type: 'docsVersionDropdown',
+              position: 'left',
+            },
+            {
+              label: 'Use-cases Library',
+              position: 'right',
+              href: 'https://www.formance.com/use-cases',
+            },
+            {
+              href: 'https://github.com/formancehq/stack',
+              label: 'GitHub',
+              position: 'right',
+            },
+            {
+              label: 'Go to Website',
+              position: 'right',
+              href: 'https://www.formance.com/',
+            },
+          ],
+        },
+        footer: {
+          style: 'light',
+          links: [
+            {
+              title: 'Documentation',
+              items: [
+                {
+                  label: 'Ledger',
+                  to: '/ledger',
+                },
+                {
+                  label: 'Payments',
+                  to: '/payments',
+                },
+                {
+                  label: 'Wallets',
+                  to: '/wallets',
+                },
+              ],
+            },
+            {
+              title: 'Community',
+              items: [
+                {
+                  label: 'Slack',
+                  href: 'https://bit.ly/formance-slack',
+                },
+                {
+                  label: 'Twitter',
+                  href: 'https://twitter.com/formancehq',
+                },
+              ],
+            },
+            {
+              title: 'More',
+              items: [
+                {
+                  label: 'GitHub',
+                  href: 'https://github.com/formancehq/stack',
+                },
+                {
+                  label: 'Cloud Status',
+                  href: 'https://status.formance.com',
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © 2021-2023 Formance, Inc`,
+        },
+        prism: {
+          theme: darkCodeTheme,
+        },
+        posthog: {
+          apiKey: 'phc_hRDv01yOHJNUM7l5SmXPUtSQUuNw4r5am9FtV83Z9om',
+          appUrl: 'https://app.posthog.com',  // optional
+          enableInDevelopment: false,  // optional
+        },
+        algolia: {
+          appId: 'IHGRMFJIIG',
+          apiKey: '7864304f16ea5f9d27b7a553c83ad17a',
+          indexName: 'numary',
 
-        // Optional: see doc section below
-        contextualSearch: true,
+          // Optional: see doc section below
+          contextualSearch: true,
 
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'docs\\.formance\\.com',
+          // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+          externalUrlRegex: 'docs\\.formance\\.com',
 
-        // Optional: Algolia search parameters
-        searchParameters: {},
+          // Optional: Algolia search parameters
+          searchParameters: {},
 
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
+          // Optional: path for search page that enabled by default (`false` to disable it)
+          searchPagePath: 'search',
 
-        //... other Algolia params
-      },
-    })
+          //... other Algolia params
+        },
+      })
 };
 
 module.exports = config;
