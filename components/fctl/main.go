47e8a2bc47e47595cd9cc54976f5a737c8feb7fa
David Ragot <david@formance.com>
2023-11-02 18:09:52 +0100

    fix: openapi version as main branch

--- components/fctl/main.go
+++ components/fctl/main.go
@@ -1,4 +1,4 @@
-//go:generate docker run --rm -w /local -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate  -i ./membership-swagger.yaml -g go -o ./membershipclient --git-user-id=formancehq --git-repo-id=fctl -p packageVersion=latest -p isGoSubmodule=true -p packageName=membershipclient
+//go:generate docker run --rm -w /local -v ${PWD}:/local openapitools/openapi-generator-cli:v6.6.0 generate  -i ./membership-swagger.yaml -g go -o ./membershipclient --git-user-id=formancehq --git-repo-id=fctl -p packageVersion=latest -p isGoSubmodule=true -p packageName=membershipclient
 //go:generate rm -rf ./membershipclient/test
 package main
 
