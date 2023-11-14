94f5754052456d1c915fa0f3aacda75a75592f96
RAGOT David <35502263+Dav-14@users.noreply.github.com>
2023-11-14 17:03:56 +0100

    feat: add command update mapping (#858)

---
+++ components/search/cmd/update_mapping.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/formancehq/search/pkg/searchengine"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func NewUpdateMapping() *cobra.Command {
+	return &cobra.Command{
+		Use:   "update-mapping",
+		Short: "Update ElasticSearch mapping",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			openSearchServiceHost := viper.GetString(openSearchServiceFlag)
+			if openSearchServiceHost == "" {
+				exitWithError(cmd.Context(), "missing open search service host")
+			}
+
+			client, err := newOpensearchClient(openSearchServiceHost)
+			if err != nil {
+				return err
+			}
+
+			esIndex := viper.GetString(esIndicesFlag)
+			if esIndex == "" {
+				return errors.New("es index not defined")
+			}
+
+			return searchengine.UpdateMapping(cmd.Context(), client, esIndex)
+		},
+	}
+}
