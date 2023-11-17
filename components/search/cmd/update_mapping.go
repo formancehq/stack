bdbc9716ed448015d8f04d14aff1055f88fd1610
RAGOT David <35502263+Dav-14@users.noreply.github.com>
2023-11-17 15:53:46 +0100

    feat: search-add-debug-log-and-fail-on-update-mapping (#875)
    Co-authored-by: David Ragot <david@formance.com>

--- components/search/cmd/update_mapping.go
+++ components/search/cmd/update_mapping.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"github.com/formancehq/search/pkg/searchengine"
+	"github.com/formancehq/stack/libs/go-libs/service"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func NewUpdateMapping() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "update-mapping",
 		Short: "Update ElasticSearch mapping",
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -30,4 +31,8 @@ func NewUpdateMapping() *cobra.Command {
 			return searchengine.UpdateMapping(cmd.Context(), client, esIndex)
 		},
 	}
+
+	cmd.Flags().Bool(service.DebugFlag, false, "debug mode")
+
+	return cmd
 }
