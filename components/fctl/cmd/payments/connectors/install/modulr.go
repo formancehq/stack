1037bb4de546d162abe396b77198b63efb6a1632
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-11-28 16:32:14 +0100

    feat(fctl): fix with new payments version

--- components/fctl/cmd/payments/connectors/install/modulr.go
+++ components/fctl/cmd/payments/connectors/install/modulr.go
@@ -15,13 +15,17 @@ import (
 type PaymentsConnectorsModulrStore struct {
 	Success       bool   `json:"success"`
 	ConnectorName string `json:"connectorName"`
+	ConnectorID   string `json:"connectorId"`
 }
+
 type PaymentsConnectorsModulrController struct {
 	store                *PaymentsConnectorsModulrStore
 	endpointFlag         string
 	defaultEndpoint      string
 	pollingPeriodFlag    string
 	defaultpollingPeriod string
+	nameFlag             string
+	defaultName          string
 }
 
 var _ fctl.Controller[*PaymentsConnectorsModulrStore] = (*PaymentsConnectorsModulrController)(nil)
@@ -39,6 +43,8 @@ func NewPaymentsConnectorsModulrController() *PaymentsConnectorsModulrController
 		defaultEndpoint:      "https://api-sandbox.modulrfinance.com",
 		pollingPeriodFlag:    "polling-period",
 		defaultpollingPeriod: "2m",
+		nameFlag:             "name",
+		defaultName:          "modulr",
 	}
 }
 
@@ -49,6 +55,7 @@ func NewModulrCommand() *cobra.Command {
 		fctl.WithArgs(cobra.ExactArgs(2)),
 		fctl.WithStringFlag(c.endpointFlag, c.defaultEndpoint, "API endpoint"),
 		fctl.WithStringFlag(c.pollingPeriodFlag, c.defaultpollingPeriod, "Polling duration"),
+		fctl.WithStringFlag(c.nameFlag, c.defaultName, "Connector name"),
 		fctl.WithController[*PaymentsConnectorsModulrStore](c),
 	)
 }
@@ -76,6 +83,7 @@ func (c *PaymentsConnectorsModulrController) Run(cmd *cobra.Command, args []stri
 	response, err := paymentsClient.Payments.InstallConnector(cmd.Context(), operations.InstallConnectorRequest{
 		ConnectorConfig: shared.ConnectorConfig{
 			ModulrConfig: &shared.ModulrConfig{
+				Name:          fctl.GetString(cmd, c.nameFlag),
 				APIKey:        args[0],
 				APISecret:     args[1],
 				Endpoint:      endpoint,
@@ -95,12 +103,15 @@ func (c *PaymentsConnectorsModulrController) Run(cmd *cobra.Command, args []stri
 	c.store.Success = true
 	c.store.ConnectorName = internal.ModulrConnector
 
+	if response.ConnectorResponse != nil {
+		c.store.ConnectorID = response.ConnectorResponse.Data.ConnectorID
+	}
+
 	return c, nil
 }
 
 func (c *PaymentsConnectorsModulrController) Render(cmd *cobra.Command, args []string) error {
-
-	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("Connector '%s' installed!", c.store.ConnectorName)
+	pterm.Success.WithWriter(cmd.OutOrStdout()).Printfln("%s: connector '%s' installed!", c.store.ConnectorName, c.store.ConnectorID)
 
 	return nil
 }
