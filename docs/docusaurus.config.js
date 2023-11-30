9f0fdad043774da8107cdf025e640df4069316dd
Maxence Maireaux <maxence@maireaux.fr>
2023-11-30 15:59:01 +0100

    docs: Update CSS & docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -70,6 +70,16 @@ const config = {
     ],
   ],
 
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
   themeConfig:
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
