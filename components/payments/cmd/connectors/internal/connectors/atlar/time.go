e2903495835fe1c3ad493d6a2b6a7f0f68248a07
Lukas Wagner <lukas.wagner@getmomo.de>
2023-12-04 17:36:01 +0100

    Fix time parsing

--- components/payments/cmd/connectors/internal/connectors/atlar/time.go
+++ components/payments/cmd/connectors/internal/connectors/atlar/time.go
@@ -3,9 +3,9 @@ package atlar
 import "time"
 
 func ParseAtlarTimestamp(value string) (time.Time, error) {
-	return time.Parse("2006-01-02T15:04:05.999999999Z", value)
+	return time.Parse(time.RFC3339Nano, value)
 }
 
 func ParseAtlarDate(value string) (time.Time, error) {
-	return time.Parse("2006-01-02", value)
+	return time.Parse(time.DateOnly, value)
 }
