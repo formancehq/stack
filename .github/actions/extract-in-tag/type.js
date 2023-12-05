ca8e88b67f581a13fc7d002d31285c7ebdd17601
Maxence Maireaux <maxence@maireaux.fr>
2023-12-05 17:45:04 +0100

    ci: little update

--- .github/actions/extract-in-tag/type.js
+++ .github/actions/extract-in-tag/type.js
@@ -1,7 +1,7 @@
 const value = process.argv[2];
 
 const getValue = (string) => {
-    const value = string.match(/([a-z].*)\/([a-z].*)\/([a-z].*)/)[2];
+    const value = string.match(/([a-z].*)\/([a-z].*)\/([a-z].*)/)[1];
     return value;
 };
 
