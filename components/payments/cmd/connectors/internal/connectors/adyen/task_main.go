640ec92d558267d58b9abe3b06356c05439c8843
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-12-11 15:55:52 +0100

    feat(payments): add adyen merchants account fetching

--- components/payments/cmd/connectors/internal/connectors/adyen/task_main.go
+++ components/payments/cmd/connectors/internal/connectors/adyen/task_main.go
@@ -2,8 +2,10 @@ package adyen
 
 import (
 	"context"
+	"errors"
 
 	"github.com/formancehq/payments/cmd/connectors/internal/task"
+	"github.com/formancehq/payments/internal/models"
 	"github.com/formancehq/stack/libs/go-libs/logging"
 )
 
@@ -14,6 +16,22 @@ func taskMain() task.Task {
 		scheduler task.Scheduler,
 	) error {
 		logger.Info(taskNameMain)
+
+		taskAccounts, err := models.EncodeTaskDescriptor(TaskDescriptor{
+			Name: "Fetch accounts from client",
+			Key:  taskNameFetchAccounts,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = scheduler.Schedule(ctx, taskAccounts, models.TaskSchedulerOptions{
+			ScheduleOption: models.OPTIONS_RUN_NOW,
+			RestartOption:  models.OPTIONS_RESTART_IF_NOT_ACTIVE,
+		})
+		if err != nil && !errors.Is(err, task.ErrAlreadyScheduled) {
+			return err
+		}
 
 		return nil
 	}
