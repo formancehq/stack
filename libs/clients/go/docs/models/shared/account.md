54cdf91cc95b930f6b84a08aaef177b361ce1266
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2023-12-13 08:52:18 +0100

    fix: missing tags

--- libs/clients/go/docs/models/shared/account.md
+++ libs/clients/go/docs/models/shared/account.md
@@ -6,5 +6,5 @@
 | Field                    | Type                     | Required                 | Description              | Example                  |
 | ------------------------ | ------------------------ | ------------------------ | ------------------------ | ------------------------ |
 | `Address`                | *string*                 | :heavy_check_mark:       | N/A                      | users:001                |
-| `Metadata`               | map[string]*interface{}* | :heavy_minus_sign:       | N/A                      |                          |
+| `Metadata`               | map[string]*interface{}* | :heavy_minus_sign:       | N/A                      | [object Object]          |
 | `Type`                   | **string*                | :heavy_minus_sign:       | N/A                      | virtual                  |
--- libs/clients/go/pkg/models/operations/countaccounts.go
+++ libs/clients/go/pkg/models/operations/countaccounts.go
@@ -7,17 +7,13 @@ import (
 	"net/http"
 )
 
-// CountAccountsMetadata - Filter accounts by metadata key value pairs. The filter can be used like this metadata[key]=value1&metadata[a.nested.key]=value2
-type CountAccountsMetadata struct {
-}
-
 type CountAccountsRequest struct {
 	// Filter accounts by address pattern (regular expression placed between ^ and $).
 	Address *string `queryParam:"style=form,explode=true,name=address"`
 	// Name of the ledger.
 	Ledger string `pathParam:"style=simple,explode=false,name=ledger"`
 	// Filter accounts by metadata key value pairs. The filter can be used like this metadata[key]=value1&metadata[a.nested.key]=value2
-	Metadata *CountAccountsMetadata `queryParam:"style=deepObject,explode=true,name=metadata"`
+	Metadata map[string]interface{} `queryParam:"style=deepObject,explode=true,name=metadata"`
 }
 
 func (o *CountAccountsRequest) GetAddress() *string {
@@ -34,7 +30,7 @@ func (o *CountAccountsRequest) GetLedger() string {
 	return o.Ledger
 }
 
-func (o *CountAccountsRequest) GetMetadata() *CountAccountsMetadata {
+func (o *CountAccountsRequest) GetMetadata() map[string]interface{} {
 	if o == nil {
 		return nil
 	}
