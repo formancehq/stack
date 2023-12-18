07a5ece96402cad03c93f22dc4d2a97dfd8ccc50
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-12-18 11:17:04 +0100

    fix(payments): fix marshal when getting connector config (#1020)

--- components/payments/cmd/connectors/internal/connectors/duration.go
+++ components/payments/cmd/connectors/internal/connectors/duration.go
@@ -10,6 +10,10 @@ type Duration struct {
 	time.Duration `json:"duration"`
 }
 
+func (d *Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.Duration.String())
+}
+
 func (d *Duration) UnmarshalJSON(b []byte) error {
 	var rawValue any
 
