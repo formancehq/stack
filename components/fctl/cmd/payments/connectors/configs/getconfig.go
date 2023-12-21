39ed8dc4a951384d3c2b57b2c33d63d42a5f4584
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-12-21 17:44:35 +0100

    feat(fctl): add atlar config view

--- components/fctl/cmd/payments/connectors/configs/getconfig.go
+++ components/fctl/cmd/payments/connectors/configs/getconfig.go
@@ -159,6 +159,8 @@ func (c *PaymentsGetConfigController) Render(cmd *cobra.Command, args []string) 
 		err = views.DisplayMangopayConfig(cmd, c.store.ConnectorConfig)
 	case internal.MoneycorpConnector:
 		err = views.DisplayMoneycorpConfig(cmd, c.store.ConnectorConfig)
+	case internal.AtlarConnector:
+		err = views.DisplayAtlarConfig(cmd, c.store.ConnectorConfig)
 	default:
 		pterm.Error.WithWriter(cmd.OutOrStderr()).Printfln("Connection unknown.")
 	}
