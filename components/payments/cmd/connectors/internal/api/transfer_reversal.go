1bf3e134a4119c59dac617b3cc465c9f8a0fa47d
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-01-22 18:23:19 +0100

    fix pre commit

--- components/payments/cmd/connectors/internal/api/transfer_reversal.go
+++ components/payments/cmd/connectors/internal/api/transfer_reversal.go
@@ -2,9 +2,7 @@ package api
 
 import (
 	"encoding/json"
-	"math/big"
 	"net/http"
-	"time"
 
 	"github.com/formancehq/payments/cmd/connectors/internal/api/backend"
 	"github.com/formancehq/payments/cmd/connectors/internal/api/service"
@@ -13,20 +11,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 )
-
-type reverseTransferInitiationResponse struct {
-	ID                  string            `json:"id"`
-	TranferInitiationID string            `json:"transferInitiationID"`
-	CreatedAt           time.Time         `json:"createdAt"`
-	UpdatedAt           time.Time         `json:"updatedAt"`
-	Reference           string            `json:"reference"`
-	Description         string            `json:"description"`
-	Amount              *big.Int          `json:"amount"`
-	Asset               string            `json:"asset"`
-	Status              string            `json:"status"`
-	Error               string            `json:"error"`
-	Metadata            map[string]string `json:"metadata"`
-}
 
 func reverseTransferInitiationHandler(b backend.ServiceBackend) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
