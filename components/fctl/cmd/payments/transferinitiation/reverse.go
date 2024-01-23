1d8b25325c4b61d22cdb43854fbacdf2b6273c3f
David Ragot <david@formance.com>
2024-01-23 11:52:28 +0100

    fix: payment package

--- components/fctl/cmd/payments/transferinitiation/reverse.go
+++ components/fctl/cmd/payments/transferinitiation/reverse.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/formancehq/fctl/cmd/payments/versions"
 	fctl "github.com/formancehq/fctl/pkg"
-	"github.com/formancehq/formance-sdk-go/pkg/models/operations"
-	"github.com/formancehq/formance-sdk-go/pkg/models/shared"
+	"github.com/formancehq/formance-sdk-go/v2/pkg/models/operations"
+	"github.com/formancehq/formance-sdk-go/v2/pkg/models/shared"
 	"github.com/pkg/errors"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
