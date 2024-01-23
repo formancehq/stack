c284f6757f705ac911384c19e2bfd951f938f177
RAGOT David <35502263+Dav-14@users.noreply.github.com>
2024-01-23 12:12:23 +0100

    fix: remove command restriction on delete list show (#1145)
    Co-authored-by: David Ragot <david@formance.com>

--- components/fctl/cmd/cloud/organizations/users/root.go
+++ components/fctl/cmd/cloud/organizations/users/root.go
@@ -1,39 +1,14 @@
 package users
 
 import (
-	"fmt"
-
 	fctl "github.com/formancehq/fctl/pkg"
 	"github.com/spf13/cobra"
-	"golang.org/x/mod/semver"
 )
 
 func NewCommand() *cobra.Command {
 	return fctl.NewMembershipCommand("users",
 		fctl.WithAliases("u"),
 		fctl.WithShortDescription("Users management"),
-		fctl.WithRunE(func(cmd *cobra.Command, args []string) error {
-			cfg, err := fctl.GetConfig(cmd)
-			if err != nil {
-				return err
-			}
-
-			apiClient, err := fctl.NewMembershipClient(cmd, cfg)
-			if err != nil {
-				return err
-			}
-
-			version := fctl.MembershipServerInfo(cmd.Context(), apiClient)
-			if !semver.IsValid(version) {
-				return nil
-			}
-
-			if semver.Compare(version, "v0.26.1") >= 0 {
-				return nil
-			}
-
-			return fmt.Errorf("unsupported membership server version: %s", version)
-		}),
 		fctl.WithChildCommands(
 			NewListCommand(),
 			NewShowCommand(),
--- components/fctl/membershipclient/docs/Role.md
+++ components/fctl/membershipclient/docs/Role.md
@@ -11,6 +11,8 @@
 
 * `GUEST` (value: `"GUEST"`)
 
+* `USER` (value: `"USER"`)
+
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
