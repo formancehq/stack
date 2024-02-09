8916692b12ba19f4a63e317582d30b0d76efd50c
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-02-09 10:13:14 +0100

    last fixes

--- components/payments/internal/storage/migrations_v1.x.go
+++ components/payments/internal/storage/migrations_v1.x.go
@@ -365,7 +365,7 @@ func registerMigrationsV1(ctx context.Context, migrator *migrations.Migrator) {
 		migrations.Migration{
 			Up: func(tx bun.Tx) error {
 				_, err := tx.Exec(`
-				ALTER TABLE accounts.bank_account_adjustments RENAME TO accounts.bank_account_related_accounts;
+				ALTER TABLE accounts.bank_account_adjustments RENAME TO bank_account_related_accounts;
 				`)
 				if err != nil {
 					return err
