4f8a00bcf2fbd927ffc4a2d4428e06ce5161138f
David Ragot <david@formance.com>
2024-02-12 16:52:08 +0100

    fix: handle sleep correctly

--- components/fctl/cmd/stack/controller.go
+++ components/fctl/cmd/stack/controller.go
@@ -30,7 +30,11 @@ func waitStackReady(cmd *cobra.Command, client *membershipclient.APIClient, prof
 			pterm.Warning.Println("Waiting for stack to be ready...")
 			return fmt.Errorf("there might a problem with the stack scheduling, retry and if the problem persists please contact the support")
 		}
-		time.Sleep(waitTime)
+		select {
+		case <-time.After(waitTime):
+		case <-cmd.Context().Done():
+			return cmd.Context().Err()
+		}
 		waitTime *= 2
 
 	}
