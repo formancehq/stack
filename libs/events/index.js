050287b3f9b54c34d3cfb6a82858b2df7aefb37d
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2024-03-01 12:17:44 +0100

    feat(events): add aggregated file for events (#1298)

--- libs/events/index.js
+++ libs/events/index.js
@@ -5,16 +5,23 @@ const yaml = require('yaml');
 
     const rawBase = await fs.readFile("./base.yaml", { encoding: 'utf8' });
     const base = yaml.parse(rawBase);
+    const aggregated = {};
 
     for(const service of await fs.readdir("services")) {
+        aggregated[service] = {};
         for(const version of await fs.readdir('services/' + service)) {
+            aggregated[service][version] = {};
             for(const event of await fs.readdir('services/' + service + '/' + version)) {
                 const rawEventData = await fs.readFile('services/' + service + '/' + version + '/' + event, { encoding: 'utf8' });
                 base.properties.payload = yaml.parse(rawEventData);
                 const directory = 'generated/' + service + '/' + version + '/';
                 await fs.mkdir(directory, { recursive: true });
                 await fs.writeFile(directory + event.replace('.yaml', '.json'), JSON.stringify(base, null, 2));
+
+                aggregated[service][version][event.replace('.yaml', '')] = base;
             }
         }
     }
+
+    await fs.writeFile('generated/all.json', JSON.stringify(aggregated, null, 2));
 })();
