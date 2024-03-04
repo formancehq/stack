e64ecb21121fe236a20b93047e50dc17aeef4c14
David Ragot <david@formance.com>
2024-03-04 18:16:52 +0100

    fix: typo

--- components/fctl/cmd/stack/delete.go
+++ components/fctl/cmd/stack/delete.go
@@ -43,7 +43,7 @@ func NewDeleteCommand() *cobra.Command {
 		fctl.WithAliases("del", "d"),
 		fctl.WithArgs(cobra.MaximumNArgs(1)),
 		fctl.WithStringFlag(stackNameFlag, "", "Stack to delete"),
-		fctl.WithBoolFlag(forceFlag, false, "Force deleting a stack without retention policy"),
+		fctl.WithBoolFlag(forceFlag, false, "Force to delete a stack without retention policy"),
 		fctl.WithController[*DeletedStackStore](NewStackDeleteController()),
 	)
 }
