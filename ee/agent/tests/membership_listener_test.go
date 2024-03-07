59976e0d2a1dd13adebcacb01822d7874cbaef92
RAGOT David <35502263+Dav-14@users.noreply.github.com>
2024-03-07 16:23:03 +0100

    fix(agent): test additional label with @ (#1313)
    Co-authored-by: David Ragot <david@formance.com>

--- ee/agent/tests/membership_listener_test.go
+++ ee/agent/tests/membership_listener_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"reflect"
@@ -16,6 +17,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
@@ -114,6 +116,31 @@ var _ = Describe("Membership listener", func() {
 				}, scheme.ParameterCodec).Do(context.Background()).Into(clients)).To(Succeed())
 				return clients.Items
 			}).Should(HaveLen(2))
+		})
+		When("Having an @ in labels", func() {
+			var patch []byte
+			var err error
+			BeforeEach(func() {
+				patch, err = json.Marshal(map[string]any{
+					"metadata": map[string]any{
+						"labels": map[string]any{
+							"formance.com/owner-email": "example@example.net",
+						},
+					},
+				})
+				Expect(err).To(BeNil())
+
+			})
+			It("Should through an error", func() {
+				err := k8sClient.Patch(types.MergePatchType).
+					Resource("Stacks").
+					Name(stack.Name).
+					Body(patch).
+					Do(context.Background()).
+					Error()
+
+				Expect(err).To(HaveOccurred())
+			})
 		})
 		It("Should have additional labels", func() {
 			Expect(stack.Labels).To(HaveKeyWithValue("formance.com/foo", "bar"))
