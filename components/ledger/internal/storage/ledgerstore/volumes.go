0343cb92624e00073e7ae3a00dbd8d6b2e244c9f
agourdel <gourdel.alexandre@gmail.com>
2024-04-19 18:20:00 +0200

    feat(ledger): Add filter Balance[Asset] to /volumes

--- components/ledger/internal/storage/ledgerstore/volumes.go
+++ components/ledger/internal/storage/ledgerstore/volumes.go
@@ -3,23 +3,44 @@ package ledgerstore
 import (
 	"context"
 	"fmt"
-
 	ledger "github.com/formancehq/ledger/internal"
 	"github.com/formancehq/stack/libs/go-libs/bun/bunpaginate"
 	lquery "github.com/formancehq/stack/libs/go-libs/query"
 	"github.com/uptrace/bun"
+	"regexp"
 )
 
-func (store *Store) volumesQueryContext(qb lquery.Builder, q GetVolumesWithBalancesQuery) (string, []any, error) {
+func (store *Store) volumesQueryContext(qb lquery.Builder, q GetVolumesWithBalancesQuery) (string, []any, bool, error) {
 
+	metadataRegex := regexp.MustCompile("metadata\\[(.+)\\]")
+	balanceRegex := regexp.MustCompile("balance\\[(.*)\\]")
 	var (
 		subQuery string
 		args     []any
 		err      error
 	)
 
+	var useMetadata bool = false
+
 	if q.Options.QueryBuilder != nil {
 		subQuery, args, err = q.Options.QueryBuilder.Build(lquery.ContextFn(func(key, operator string, value any) (string, []any, error) {
+
+			convertOperatorToSQL := func() string {
+				switch operator {
+				case "$match":
+					return "="
+				case "$lt":
+					return "<"
+				case "$gt":
+					return ">"
+				case "$lte":
+					return "<="
+				case "$gte":
+					return ">="
+				}
+				panic("unreachable")
+			}
+
 			switch {
 			case key == "account":
 				// TODO: Should allow comparison operator only if segments not used
@@ -37,6 +58,7 @@ func (store *Store) volumesQueryContext(qb lquery.Builder, q GetVolumesWithBalan
 				if operator != "$match" {
 					return "", nil, newErrInvalidQuery("'metadata' column can only be used with $match")
 				}
+				useMetadata = true
 				match := metadataRegex.FindAllStringSubmatch(key, 3)
 				key := "accounts.metadata"
 
@@ -50,20 +72,24 @@ func (store *Store) volumesQueryContext(qb lquery.Builder, q GetVolumesWithBalan
 				key := "accounts.metadata"
 
 				return fmt.Sprintf("%s -> ? IS NOT NULL", key), []any{value}, nil
+			case balanceRegex.Match([]byte(key)):
+				match := balanceRegex.FindAllStringSubmatch(key, 2)
+				println(match)
+				return fmt.Sprintf(`balance %s ?  and asset = ?`, convertOperatorToSQL()), []any{value, match[0][1]}, nil
 			default:
 				return "", nil, newErrInvalidQuery("unknown key '%s' when building query", key)
 			}
 		}))
 		if err != nil {
-			return "", nil, err
+			return "", nil, false, err
 		}
 	}
 
-	return subQuery, args, nil
+	return subQuery, args, useMetadata, nil
 
 }
 
-func (store *Store) buildVolumesWithBalancesQuery(query *bun.SelectQuery, q GetVolumesWithBalancesQuery, where string, args []any) *bun.SelectQuery {
+func (store *Store) buildVolumesWithBalancesQuery(query *bun.SelectQuery, q GetVolumesWithBalancesQuery, where string, args []any, useMetadata bool) *bun.SelectQuery {
 
 	filtersForVolumes := q.Options.Options
 	dateFilterColumn := "effective_date"
@@ -73,45 +99,63 @@ func (store *Store) buildVolumesWithBalancesQuery(query *bun.SelectQuery, q GetV
 	}
 
 	query = query.
+		Column("account_address_array").
+		Column("account_address").
 		Column("asset").
 		ColumnExpr("sum(case when not is_source then amount else 0 end) as input").
 		ColumnExpr("sum(case when is_source then amount else 0 end) as output").
 		ColumnExpr("sum(case when not is_source then amount else -amount end) as balance").
 		Table("moves")
 
-	if filtersForVolumes.GroupLvl > 0 {
-		query = query.ColumnExpr(fmt.Sprintf(`(array_to_string((string_to_array(account_address, ':'))[1:LEAST(array_length(string_to_array(account_address, ':'),1),%d)],':')) as account`, filtersForVolumes.GroupLvl))
-	} else {
-		query = query.ColumnExpr("account_address as account")
-	}
-
-	if where != "" {
+	if useMetadata {
 		query = query.
 			Join(`join lateral (	
-			select metadata
-			from accounts a 
-			where a.seq = moves.accounts_seq
-		) accounts on true`).
-			Where(where, args...)
+		select metadata
+		from accounts a 
+		where a.seq = moves.accounts_seq
+		) accounts on true`)
 	}
 
 	query = query.
 		Where("ledger = ?", store.name).
 		Apply(filterPIT(filtersForVolumes.PIT, dateFilterColumn)).
 		Apply(filterOOT(filtersForVolumes.OOT, dateFilterColumn)).
-		GroupExpr("account, asset")
+		GroupExpr("account_address, account_address_array, asset")
 
-	return query
+	globalQuery := query.NewSelect()
+	globalQuery = globalQuery.
+		With("query", query).
+		TableExpr("query")
+
+	if filtersForVolumes.GroupLvl > 0 {
+		globalQuery = globalQuery.
+			ColumnExpr(fmt.Sprintf(`(array_to_string((string_to_array(account_address, ':'))[1:LEAST(array_length(string_to_array(account_address, ':'),1),%d)],':')) as account`, filtersForVolumes.GroupLvl)).
+			Column("asset").
+			ColumnExpr("sum(input) as input").
+			ColumnExpr("sum(output) as output").
+			ColumnExpr("sum(balance) as balance").
+			GroupExpr("account, asset")
+	} else {
+		globalQuery = globalQuery.ColumnExpr("account_address as account, asset, input, output, balance")
+	}
+
+	if where != "" {
+		globalQuery.Where(where, args...)
+	}
+
+	println(fmt.Sprintf("%s", globalQuery))
+	return globalQuery
 }
 
 func (store *Store) GetVolumesWithBalances(ctx context.Context, q GetVolumesWithBalancesQuery) (*bunpaginate.Cursor[ledger.VolumesWithBalanceByAssetByAccount], error) {
 	var (
-		where string
-		args  []any
-		err   error
+		where       string
+		args        []any
+		err         error
+		useMetadata bool
 	)
 	if q.Options.QueryBuilder != nil {
-		where, args, err = store.volumesQueryContext(q.Options.QueryBuilder, q)
+		where, args, useMetadata, err = store.volumesQueryContext(q.Options.QueryBuilder, q)
 		if err != nil {
 			return nil, err
 		}
@@ -120,7 +164,7 @@ func (store *Store) GetVolumesWithBalances(ctx context.Context, q GetVolumesWith
 	return paginateWithOffsetWithoutModel[PaginatedQueryOptions[FiltersForVolumes], ledger.VolumesWithBalanceByAssetByAccount](
 		store, ctx, (*bunpaginate.OffsetPaginatedQuery[PaginatedQueryOptions[FiltersForVolumes]])(&q),
 		func(query *bun.SelectQuery) *bun.SelectQuery {
-			return store.buildVolumesWithBalancesQuery(query, q, where, args)
+			return store.buildVolumesWithBalancesQuery(query, q, where, args, useMetadata)
 		},
 	)
 }
