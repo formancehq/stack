85f05186ac5958595f63580c3173a12bf83d65d7
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-04-25 17:47:40 +0200

    fix(*): fix duplicate flags definition (#1454)

--- ee/wallets/cmd/root.go
+++ ee/wallets/cmd/root.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/formancehq/stack/libs/go-libs/auth"
 	"github.com/formancehq/stack/libs/go-libs/otlp/otlptraces"
-	"github.com/formancehq/stack/libs/go-libs/service"
 	"github.com/spf13/cobra"
 )
 
@@ -31,7 +30,6 @@ func NewRootCommand() *cobra.Command {
 	cobra.EnableTraverseRunHooks = true
 
 	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	cmd.PersistentFlags().BoolP(service.DebugFlag, "d", false, "Debug mode")
 	serverCmd := newServeCommand()
 	auth.InitAuthFlags(serverCmd.Flags())
 	otlptraces.InitOTLPTracesFlags(serverCmd.Flags())
