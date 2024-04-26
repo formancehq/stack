c793b7b4c3c26d7b639b4c80a6f0a1c30ef17a0b
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-04-26 12:10:43 +0200

    fix(operator): make broker consumers listening broker

--- components/operator/internal/resources/brokerconsumers/init.go
+++ components/operator/internal/resources/brokerconsumers/init.go
@@ -3,6 +3,7 @@ package brokerconsumers
 import (
 	"github.com/formancehq/operator/api/formance.com/v1beta1"
 	"github.com/formancehq/operator/internal/core"
+	"github.com/formancehq/operator/internal/resources/brokers"
 	v1 "k8s.io/api/batch/v1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 )
@@ -12,6 +13,7 @@ func init() {
 		core.WithStackDependencyReconciler(Reconcile,
 			core.WithOwn[*v1beta1.BrokerConsumer](&v1beta1.BrokerTopic{}, builder.MatchEveryOwner),
 			core.WithOwn[*v1beta1.BrokerConsumer](&v1.Job{}),
+			brokers.Watch[*v1beta1.BrokerConsumer](),
 		),
 	)
 }
