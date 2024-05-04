191a548991960ec94f8946f91daac2aa07c323e6
Alessandro Scandone <alescandone@gmail.com>
2024-05-04 15:54:35 +0200

    added test

--- components/ledger/internal/machine/interpreter/parse_ast_test.go
+++ components/ledger/internal/machine/interpreter/parse_ast_test.go
@@ -88,3 +88,32 @@ func TestParseAllottedSource(t *testing.T) {
 
 	require.Equal(t, &expected, got.Program, "Program should be the same.")
 }
+
+func TestParseAllottedPercSource(t *testing.T) {
+	got := CompileFull(`send [COIN 42] (
+		source = {
+			80% from @s1
+			20% from @s2
+		}
+		destination = @dest	
+	)`)
+
+	if len(got.Errors) != 0 {
+		t.Fatalf(`Unexpected compilation errors = %#v`, got.Errors)
+	}
+
+	expected := Program{
+		Statements: []SendStatement{
+			{
+				Amount: 42,
+				Source: &AllottedSrc{[]Allotment[Source]{
+					{*big.NewRat(80, 100), &AccountSrc{Name: "s1"}},
+					{*big.NewRat(20, 100), &AccountSrc{Name: "s2"}},
+				}},
+				Destination: &AccountDest{"dest"},
+			},
+		},
+	}
+
+	require.Equal(t, &expected, got.Program, "Program should be the same.")
+}
