1ef46df008baefedde7e1af2030af9ee885e4ca3
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-05-13 17:36:47 +0200

    fix: ci

--- .github/actions/find-directory/index.js
+++ .github/actions/find-directory/index.js
@@ -12,8 +12,7 @@ function findDockerFile(dir) {
             results = results.concat(findDockerFile(file));
         } else {
             if (path.basename(file) === ".goreleaser.yml") {
-                const obj = { component: path.basename(path.dirname(file)), type: path.basename(path.dirname(path.dirname(file))) };
-                results.push(obj);
+                results.push(path.relative(".", path.dirname(file)));
             }
         }
     });
