e4105c9698cf0f406f093b1635d248965e01ff5a
Lukas Wagner <91877506+lwagner-getmomo@users.noreply.github.com>
2024-07-15 16:40:08 +0200

    feat(payments): Atlar connector: Add more bank details to Accounts (internal) and Payments (#1607)

--- components/payments/cmd/connectors/internal/connectors/atlar/task_payments.go
+++ components/payments/cmd/connectors/internal/connectors/atlar/task_payments.go
@@ -233,6 +233,7 @@ func UpdatePaymentStatusTask(
 			err = ingestAtlarTransaction(ctx,
 				ingester,
 				connectorID,
+				taskID,
 				client,
 				getCreditTransferResponse.Payload.Reconciliation.BookedTransactionID,
 			)
@@ -356,17 +357,30 @@ func ingestAtlarTransaction(
 	ctx context.Context,
 	ingester ingestion.Ingester,
 	connectorID models.ConnectorID,
+	taskID models.TaskID,
 	client *client.Client,
 	transactionId string,
 ) error {
+	ctx, span := connectors.StartSpan(
+		ctx,
+		"atlar.taskUpdatePaymentStatus.ingestAtlarTransaction",
+		attribute.String("connectorID", connectorID.String()),
+		attribute.String("taskID", taskID.String()),
+		attribute.String("transactionID", transactionId),
+	)
+	defer span.End()
 
-	transactionResponse, err := client.GetV1TransactionsID(ctx, transactionId)
+	requestCtx, cancel := contextutil.DetachedWithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	transactionResponse, err := client.GetV1TransactionsID(requestCtx, transactionId)
 	if err != nil {
+		otel.RecordError(span, err)
 		return err
 	}
 
-	batchElement, err := atlarTransactionToPaymentBatchElement(connectorID, transactionResponse.Payload)
+	batchElement, err := atlarTransactionToPaymentBatchElement(ctx, connectorID, taskID, transactionResponse.Payload, client)
 	if err != nil {
+		otel.RecordError(span, err)
 		return err
 	}
 	if batchElement == nil {
@@ -377,6 +391,7 @@ func ingestAtlarTransaction(
 
 	err = ingester.IngestPayments(ctx, batch)
 	if err != nil {
+		otel.RecordError(span, err)
 		return err
 	}
 
