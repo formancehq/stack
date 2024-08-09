9610bff3210d955fea280e1dfd30dc8a604626a2
Clément Salaün <salaun.clement@gmail.com>
2024-08-09 16:28:54 +0200

    feat: update schema

--- components/payments/cmd/connectors/internal/connectors/fawry/client/invoice.go
+++
@@ -1,8 +0,0 @@
-package client
-
-type InvoiceInfo struct {
-	Number            string `json:"number"`
-	BusinessRefNumber string `json:"businessRefNumber"`
-	DueDate           string `json:"dueDate"`
-	ExpiryDate        uint64 `json:"expiryDate"`
-}
--- components/payments/cmd/connectors/internal/connectors/fawry/client/notification.go
+++ components/payments/cmd/connectors/internal/connectors/fawry/client/notification.go
@@ -1,14 +1,17 @@
 package client
 
 type Notification struct {
-	RequestID         string      `json:"requestId"`
-	FawryRefNumber    string      `json:"fawryRefNumber"`
-	MerchantRefNumber string      `json:"merchantRefNumber"`
-	PaymentAmount     string      `json:"paymentAmount"`
-	OrderAmount       string      `json:"orderAmount"`
-	FawryFees         string      `json:"fawryFees"`
-	OrderStatus       string      `json:"orderStatus"`
-	PaymentMethod     string      `json:"paymentMethod"`
-	MessageSignature  string      `json:"messageSignature"`
-	InvoiceInfo       InvoiceInfo `json:"invoiceInfo"`
+	RequestID string `json:"requestId"`
+
+	Amount string `json:"amount"`
+
+	PaymentID     string `json:"paymentId"`
+	TransactionID string `json:"transactionId"`
+
+	BillingAccount       string `json:"billingAcct"`
+	ExtraBillingAccounts any    `json:"extraBillingAccts"`
+
+	CustomerReferenceNumber string `json:"customerReferenceNumber"`
+	TerminalID              string `json:"terminalId"`
+	BillNumber              string `json:"billNumber"`
 }
