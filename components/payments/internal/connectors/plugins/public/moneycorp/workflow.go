e87e0d9c2e90dcf5de72c8d2b236d0a0a1a279df
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-09-13 10:40:14 +0200

    feat(payments): add moneycorp plugin

---
+++ components/payments/internal/connectors/plugins/public/moneycorp/workflow.go
@@ -0,0 +1,33 @@
+package moneycorp
+
+import "github.com/formancehq/payments/internal/models"
+
+func workflow() models.Tasks {
+	return []models.TaskTree{
+		{
+			TaskType:     models.TASK_FETCH_ACCOUNTS,
+			Name:         "fetch_accounts",
+			Periodically: true,
+			NextTasks: []models.TaskTree{
+				{
+					TaskType:     models.TASK_FETCH_BALANCES,
+					Name:         "fetch_balances",
+					Periodically: true,
+					NextTasks:    []models.TaskTree{},
+				},
+				{
+					TaskType:     models.TASK_FETCH_PAYMENTS,
+					Name:         "fetch_payments",
+					Periodically: true,
+					NextTasks:    []models.TaskTree{},
+				},
+				{
+					TaskType:     models.TASK_FETCH_EXTERNAL_ACCOUNTS,
+					Name:         "fetch_recipients",
+					Periodically: true,
+					NextTasks:    []models.TaskTree{},
+				},
+			},
+		},
+	}
+}
