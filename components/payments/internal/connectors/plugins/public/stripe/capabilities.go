51c72090fc6a5d2b08ce3ede912e7e2fd3e0b400
Crimson Thompson <crimson@formance.com>
2024-09-30 09:31:57 +0200

    feat(payments): update Stripe capabilities

--- components/payments/internal/connectors/plugins/public/stripe/capabilities.go
+++ components/payments/internal/connectors/plugins/public/stripe/capabilities.go
@@ -4,4 +4,7 @@ import "github.com/formancehq/payments/internal/models"
 
 var capabilities = []models.Capability{
 	models.CAPABILITY_FETCH_ACCOUNTS,
+	models.CAPABILITY_FETCH_EXTERNAL_ACCOUNTS,
+	models.CAPABILITY_FETCH_PAYMENTS,
+	models.CAPABILITY_FETCH_OTHERS,
 }
